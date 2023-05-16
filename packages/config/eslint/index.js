6bffe5d593821d049d8bdf0ef8eede1c058aad91
Sergiy Dybskiy <sergiy@berachain.com>
2023-05-16 18:00:41 -0400

    rule

--- packages/config/eslint/index.js
+++ packages/config/eslint/index.js
@@ -10,6 +10,7 @@ const config = {
   rules: {
     "@typescript-eslint/no-unsafe-argument": "off",
     "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-return": "off",
     "@next/next/no-html-link-for-pages": "off",
     "@typescript-eslint/restrict-template-expressions": "off",
     "@typescript-eslint/no-unused-vars": [
