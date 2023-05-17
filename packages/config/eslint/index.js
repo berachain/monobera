ae4903015607330798ffacca000f67795b148636
Sergiy Dybskiy <sergiy@berachain.com>
2023-05-17 11:59:43 -0400

    ts rules

--- packages/config/eslint/index.js
+++ packages/config/eslint/index.js
@@ -11,6 +11,8 @@ const config = {
     "@typescript-eslint/no-unsafe-argument": "off",
     "@typescript-eslint/no-unsafe-assignment": "off",
     "@typescript-eslint/no-unsafe-return": "off",
+    "@typescript-eslint/no-unsafe-call": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
     "@next/next/no-html-link-for-pages": "off",
     "@typescript-eslint/restrict-template-expressions": "off",
     "@typescript-eslint/no-unused-vars": [
