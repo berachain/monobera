cabeaaa96d13690c59d88822135bac5aa7345332
Sergiy Dybskiy <sergiy@berachain.com>
2023-05-23 16:53:07 -0400

    Pool page WIP

--- apps/dapp/src/app/pool/page.tsx
+++ apps/dapp/src/app/pool/page.tsx
@@ -1,9 +1,16 @@
+import { Card } from "@bera/ui/card";
+
+import PoolsTable from "~/components/pools-table";
+
 export const runtime = "edge";
 
 export default function Pool() {
   return (
-    <div className="m-auto flex w-full max-w-4xl flex-col items-center justify-center gap-5">
-      <h1>Pool page</h1>
+    <div className="m-auto flex w-full flex-col gap-5">
+      <h1 className="text-left text-2xl font-semibold">Pool page</h1>
+      <Card className="w-full">
+        <PoolsTable />
+      </Card>
     </div>
   );
 }
