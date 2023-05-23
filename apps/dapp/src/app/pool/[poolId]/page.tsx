912fe03806e80bcf2bec30c4c8687cc6f33dda69
Sergiy Dybskiy <sergiy@berachain.com>
2023-05-23 18:08:08 -0400

    Pool individual page WIP

--- apps/dapp/src/app/pool/[poolId]/page.tsx
+++ apps/dapp/src/app/pool/[poolId]/page.tsx
@@ -1,5 +1,23 @@
 import React from "react";
+import dynamic from "next/dynamic";
+import { Card, CardContent } from "@bera/ui/card";
+
+import { generateDataForPast90Days } from "~/utils/generateData";
+
+const DynamicChart = dynamic(() => import("~/components/chart"), {
+  loading: () => <p>Loading...</p>,
+  ssr: false,
+});
 
 export default function PoolPage({ params }: { params: { poolId: string } }) {
-  return <div>{params.poolId}</div>;
+  return (
+    <div className="m-auto flex w-full flex-col gap-5">
+      <h1 className="text-left text-2xl font-semibold">{params.poolId}</h1>
+      <Card>
+        <CardContent className="p-4">
+          <DynamicChart chartData={generateDataForPast90Days()} type="bar" />
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
