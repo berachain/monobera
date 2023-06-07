36ae73b34566c94edd7de1a94476bf0eef08f128
Garrett Parris <garrettp18@gmail.com>
2023-06-07 10:49:02 -0400

    add toasts and txn history in a useTxn wrapper

--- apps/bgt-station/src/components/bgt-activity.tsx
+++ apps/bgt-station/src/components/bgt-activity.tsx
@@ -1,57 +1,42 @@
 import React from "react";
+import {
+  truncateHash,
+  useRecentTransactions,
+  type NewTransaction,
+} from "@bera/berajs";
 
-interface GovernanceActivity {
-  delegationAmount: number;
-  currencyName: string;
-  validatorName: string;
-  transactionHash: string;
-  timestamp: number;
+function formatTimestamp(timestamp: number): string {
+  const date = new Date(timestamp);
+
+  const formattedTimestamp = date.toLocaleString(undefined, {
+    month: "2-digit",
+    day: "2-digit",
+    year: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+  });
+
+  return formattedTimestamp;
 }
 
-const governanceActivities: GovernanceActivity[] = [
-  {
-    delegationAmount: 100,
-    currencyName: "BGT",
-    validatorName: "Validator A",
-    transactionHash: "0xabc123",
-    timestamp: 654320000, // Example timestamp (Unix epoch time in seconds)
-  },
-  {
-    delegationAmount: 200,
-    currencyName: "BGT",
-    validatorName: "Validator B",
-    transactionHash: "0xdef456",
-    timestamp: 1654330000, // Example timestamp (Unix epoch time in seconds)
-  },
-  {
-    delegationAmount: 150,
-    currencyName: "BGT",
-    validatorName: "Validator C",
-    transactionHash: "0xghi789",
-    timestamp: 1654340000, // Example timestamp (Unix epoch time in seconds)
-  },
-  // Add more governance activities as needed
-];
-
-export function BgtActivity() {
+export function History() {
+  const transactions = useRecentTransactions();
   return (
     <div className="grid gap-4">
       <h3 className="text-xl font-medium">History</h3>
-      {governanceActivities.map((activity) => (
+      {transactions.map((txn: NewTransaction) => (
         <div
           className="flex justify-between border-t border-backgroundSecondary pt-2"
-          key={activity.transactionHash}
+          key={txn.hash}
         >
           <div className="text-xs">
-            <p className="font-medium">
-              Delegated {activity.delegationAmount} {activity.currencyName} to{" "}
-              {activity.validatorName}
-            </p>
+            <p className="font-medium">{txn.description}</p>
             <p className="text-backgroundSecondary">
-              {activity.transactionHash}
+              {truncateHash(txn.hash as `0x${string}`)}
             </p>
           </div>
-          <div className="text-xs">4:20</div>
+          <div className="text-xs">{formatTimestamp(txn.timestamp)}</div>
         </div>
       ))}
     </div>
