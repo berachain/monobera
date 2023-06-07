228394f6ef08c91fa7d717679dacdc19a2924421
Garrett Parris <garrettp18@gmail.com>
2023-06-07 11:42:43 -0400

    add jsDoc to useTxn hook

--- apps/bgt-station/src/hooks/usTxn.tsx
+++ apps/bgt-station/src/hooks/usTxn.tsx
@@ -1,13 +1,43 @@
 import { useEffect, useState } from "react";
-import { useAddRecentTransaction, useBeraContractWrite } from "@bera/berajs";
+import { useAddRecentTransaction, useBeraContractWrite, type IContractWrite } from "@bera/berajs";
 import toast from "react-hot-toast";
 
-interface useTxnApi {
+interface IUseTxn {
   message?: string;
 }
 
-export const useTxn = ({ message = "" }: useTxnApi) => {
+interface UseTxnApi {
+  write: (props: IContractWrite) => void;
+  isLoading: boolean;
+  isSuccess: boolean;
+  isError: boolean;
+}
+
+/**
+ * @typedef {Object} IUseTxn
+ * @property {string} [message] - The message to be displayed in the toast and transaction history.
+ */
+
+/**
+ * @typedef {Object} UseTxnApi
+ * @property {Function} write - Function to initiate a contract write operation.
+ * @property {boolean} isLoading - Indicates if the transaction is currently loading.
+ * @property {boolean} isSuccess - Indicates if the transaction was successful.
+ * @property {boolean} isError - Indicates if the transaction encountered an error.
+ */
+
+/**
+ * Custom React hook for handling transactions with toasts and transaction history.
+ *
+ * @param {IUseTxn} options - Options for the hook.
+ * @returns {UseTxnApi} - An object containing transaction-related properties and functions.
+ */
+export const useTxn = ({ message = "" }: IUseTxn): UseTxnApi => {
   const [identifier, setIdentifier] = useState("");
+
+  /**
+   * Generate a unique identifier for the transaction based on the message.
+   */
   useEffect(() => {
     setIdentifier(message + Math.random().toString(6));
   }, [message]);
@@ -15,15 +45,26 @@ export const useTxn = ({ message = "" }: useTxnApi) => {
   const addRecentTransaction = useAddRecentTransaction();
 
   const { write, isLoading, isSuccess, isError } = useBeraContractWrite({
+    /**
+     * Error callback function executed when a transaction encounters an error.
+     *
+     * @param {Error} error - The error object.
+     */
     onError: (error) => {
       console.log(error);
       toast.remove(`loading-${identifier}`);
       toast.remove(`submission-${identifier}`);
-      toast.error("error!", {
+      toast.error("Error!", {
         duration: 3000,
         id: `error-${identifier}`,
       });
     },
+
+    /**
+     * Success callback function executed when a transaction is successful.
+     *
+     * @param {string} result - The transaction hash or result.
+     */
     onSuccess: (result) => {
       console.log(result);
       toast.remove(`submission-${identifier}`);
@@ -37,12 +78,20 @@ export const useTxn = ({ message = "" }: useTxnApi) => {
         timestamp: Date.now(),
       });
     },
+
+    /**
+     * Loading callback function executed when a transaction is loading or in progress.
+     */
     onLoading: () => {
       toast.loading("Waiting for wallet action.", {
         icon: "⏳",
         id: `loading-${identifier}`,
       });
     },
+
+    /**
+     * Submission callback function executed when a transaction is submitted.
+     */
     onSubmission: () => {
       toast.remove(`loading-${identifier}`);
       toast("Transaction submitted!", {
--- packages/berajs/src/hooks/useContractWrite/index.ts
+++ packages/berajs/src/hooks/useContractWrite/index.ts
@@ -1,4 +1,5 @@
 export * from "./error";
+export * from './types'
 export { default as useBeraContractWrite } from "./useBeraContractWrite";
 export { default as useKeplrContractWrite } from "./useKeplrContractWrite";
 export { default as useContractWrite } from "./useContractWrite";
