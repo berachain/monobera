70bd0528cc11ba7b0e1a75495925b32bb73602d5
Sergiy Dybskiy <sergiy@berachain.com>
2023-06-07 16:30:46 -0400

    imports

--- apps/bgt-station/src/app/dashboard/components/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./CuttingBoard";
-export * from "./Details";
-export * from "./EpochTimeline";
-export * from "./Stats";
-export * from "./Validators";
-export * from "./Supply";
-export * from "./CircleStat";
