b6a758f8a324231b97f82983f7b6723c1b4b69c8
Sergiy Dybskiy <sergiy@berachain.com>
2023-06-12 19:01:13 -0400

    tsup: heap test

--- packages/berajs/tsup.config.ts
+++ packages/berajs/tsup.config.ts
@@ -2,7 +2,7 @@ const config = {
   entryPoints: ["src/index.ts"], // The entry point(s) of your library
   // format: ["cjs", "esm"], // The desired output format(s)
   minify: true, // Whether to minify the output
-  sourcemap: true, // Whether to generate sourcemaps
+  // sourcemap: true, // Whether to generate sourcemaps
   // splitting: true, // Whether to split the bundle into chunks
   dts: true, // Whether to generate TypeScript declaration files
   // target: "node18", // Specify your target environment
