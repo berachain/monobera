6929154ec05f729a4ff603f5fa10a87238b0ae95
Garrett Parris <garrettp18@gmail.com>
2023-06-15 14:39:56 -0400

    ReeeeEEEE

--- packages/berajs/src/api/contracts/index.ts
+++ packages/berajs/src/api/contracts/index.ts
@@ -1,12 +1,12 @@
 import { isProduction } from "../utils/isProduction";
 
 const TESTNET_CONTRACTS = {
-  multicall: "0x156fB7c7Ffa86F5df4CdC109e74EeBdC4A947249",
+  multicall: "0xF44791eCf779318C22d6eA7fbb4741aA3b167654",
   dummy: "0x459C653FaAE6E13b59cf8E005F5f709C7b2c2EB4",
 };
 
 const MAINNET_CONTRACTS = {
-  multicall: "0x0c26395cc532E635Dd452bb7a9b2A5D104AC1783",
+  multicall: "0xF44791eCf779318C22d6eA7fbb4741aA3b167654",
 };
 
 export const getContracts = () => {
