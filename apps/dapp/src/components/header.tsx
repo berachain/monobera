f6c17e7e2bee6a0b504290ce79a2fc56b81bd311
Sergiy Dybskiy <sergiy@berachain.com>
2023-06-21 14:53:43 -0400

    remove tsup

--- apps/dapp/src/components/header.tsx
+++ apps/dapp/src/components/header.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import dynamic from "next/dynamic";
 import Link from "next/link";
-import { HistoryPopover } from "@bera/shared-ui";
+import { HistoryPopover, Wallet } from "@bera/shared-ui";
 import { Icons } from "@bera/ui/icons";
 
 import { MainNav } from "./main-nav";
 import { MobileDropdown } from "./mobile-nav";
-import { Wallet } from "./wallet";
 
 const ThemeToggle = dynamic(() => import("~/components/theme-toggle"), {
   ssr: true,
