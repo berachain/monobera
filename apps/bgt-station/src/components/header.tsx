9205ff54aafa38fdfcbb9b9952a645bec94fe533
Sergiy Dybskiy <sergiy@berachain.com>
2023-06-20 20:21:09 -0400

    Shared UI

--- apps/bgt-station/src/components/header.tsx
+++ apps/bgt-station/src/components/header.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import dynamic from "next/dynamic";
 import Link from "next/link";
+import { HistoryPopover } from "@bera/shared-ui";
 import { Icons } from "@bera/ui/icons";
 
-import { BgtControls } from "./bgt-controls";
 import { MainNav } from "./main-nav";
 import { MobileDropdown } from "./mobile-nav";
 import { Wallet } from "./wallet";
@@ -25,7 +25,7 @@ export function Header() {
       </div>
       <nav className="fixed left-0 right-0 z-50 lg:left-full lg:bg-transparent">
         <div className="mx-auto flex h-16 w-full items-center justify-between px-4 lg:justify-end">
-          <BgtControls />
+          <HistoryPopover />
           <MobileDropdown />
           <div className="flex gap-2 lg:mr-8">
             <ThemeToggle />
