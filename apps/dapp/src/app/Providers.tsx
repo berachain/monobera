07660889e2f202a98172f71b491d92b36920981d
Sergiy Dybskiy <sergiy@berachain.com>
2023-06-27 15:03:01 -0400

    default config

--- apps/dapp/src/app/Providers.tsx
+++ apps/dapp/src/app/Providers.tsx
@@ -4,11 +4,10 @@ import React from "react";
 import { BeraConfig } from "@bera/berajs";
 
 import { ThemeProvider } from "~/components/theme-provider";
-import { beraConfig } from "~/config/beraJs";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <BeraConfig networkConfig={beraConfig} autoConnect={true}>
+    <BeraConfig autoConnect={true}>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
         {children}
       </ThemeProvider>
