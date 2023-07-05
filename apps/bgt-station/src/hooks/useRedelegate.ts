061700ee289a66e567389a8790b2ea8a6d760a1f
Sergiy Dybskiy <sergiy@berachain.com>
2023-07-05 00:57:12 -0400

    BGT Redelegate functionality

---
+++ apps/bgt-station/src/hooks/useRedelegate.ts
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import {
+  BeravaloperToEth,
+  STAKING_PRECOMPILE_ABI,
+  STAKING_PRECOMPILE_ADDRESS,
+  getTokens,
+  usePollAccountDelegations,
+  usePollActiveValidators,
+  usePollAssetWalletBalance,
+  type Validator,
+} from "@bera/berajs";
+import { parseUnits } from "viem";
+
+import { useTxn } from "./useTxn";
+
+export const useRedelegate = (fromAddress: `0x{string}`) => {
+  console.log("useRedelegate");
+  const [redelegateAmount, setRedelegateAmount] = useState(0);
+  const [dstValidator, setDstValidator] = useState<Validator | null>(null);
+  const { useActiveValidator } = usePollActiveValidators();
+  const srcValidator = useActiveValidator(fromAddress);
+  usePollAssetWalletBalance();
+  const tokens = getTokens();
+  const { useSelectedAccountDelegation } =
+    usePollAccountDelegations(fromAddress);
+  const accountDelegation = useSelectedAccountDelegation();
+  const { write, isLoading } = useTxn({
+    message: `redelegate ${redelegateAmount} BGT from ${fromAddress} to ${dstValidator?.operatorAddress}`,
+    onSuccess: () => console.log("success"),
+    onError: () => console.log("error"),
+  });
+
+  const redelegate = () => {
+    if (dstValidator && srcValidator) {
+      console.log(
+        "useRedelegate",
+        BeravaloperToEth(srcValidator.operatorAddress).toLowerCase(),
+        BeravaloperToEth(dstValidator.operatorAddress).toLowerCase(),
+        parseUnits(`${redelegateAmount}`, 18),
+      );
+      try {
+        write({
+          address: STAKING_PRECOMPILE_ADDRESS,
+          abi: STAKING_PRECOMPILE_ABI,
+          functionName: "beginRedelegate",
+          params: [
+            BeravaloperToEth(srcValidator.operatorAddress).toLowerCase(),
+            BeravaloperToEth(dstValidator.operatorAddress).toLowerCase(),
+            parseUnits(`${redelegateAmount}`, 18),
+          ],
+        });
+      } catch (error) {
+        throw new Error("Redelegate error", error as Error);
+      }
+    }
+  };
+
+  return {
+    redelegateAmount,
+    setRedelegateAmount,
+    dstValidator,
+    setDstValidator,
+    srcValidator,
+    isLoading,
+    accountDelegation,
+    redelegate,
+    tokens,
+  };
+};
