3db35e07970118fd9338067ab172f8b7665f89b2
Sergiy Dybskiy <sergiy@berachain.com>
2023-07-18 18:45:35 -0400

    mobile nav styling

--- apps/dex/src/components/main-nav.tsx
+++ apps/dex/src/components/main-nav.tsx
@@ -34,7 +34,7 @@ export function MainNav({
             <NavigationMenu key={item.href}>
               <NavigationMenuList>
                 <NavigationMenuItem>
-                  <NavigationMenuTrigger>Components</NavigationMenuTrigger>
+                  <NavigationMenuTrigger>{item.title}</NavigationMenuTrigger>
                   <NavigationMenuContent>
                     <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
                       {item.children.map((component) => (
@@ -81,7 +81,7 @@ const ListItem = forwardRef<
           target="_blank"
           ref={ref}
           className={cn(
-            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-primary focus:bg-accent focus:text-primary",
             className,
           )}
           {...props}
