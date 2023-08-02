d0dd4729f9f3b76201d5e84129a311c11db834ab
Sergiy Dybskiy <sergiy@berachain.com>
2023-08-02 09:14:10 -0400

    hot pools

--- apps/dex/src/app/homepage/components/HotPools.tsx
+++ apps/dex/src/app/homepage/components/HotPools.tsx
@@ -3,8 +3,12 @@ import React from "react";
 export default function HotPools() {
   return (
     <section className="my-24">
-      <h2>Hottest Yield in Defi</h2>
-      <h3>Leverage our boosted yields to increase your rewards</h3>
+      <div className="text-center">
+        <h2 className="mb-6 text-5xl font-extrabold">Hottest Yield in Defi</h2>
+        <h3 className="text-xl font-semibold text-muted-foreground">
+          Leverage our boosted yields to increase your rewards
+        </h3>
+      </div>
     </section>
   );
 }
