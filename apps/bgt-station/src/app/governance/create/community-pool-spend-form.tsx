848ea90b7f01ef3c345ecd681a90e7d765a35b61
System Administrator <root@ninas-MacBook-Pro.local>
2023-08-08 17:32:48 -0400

    form for community

--- apps/bgt-station/src/app/governance/create/community-pool-spend-form.tsx
+++ apps/bgt-station/src/app/governance/create/community-pool-spend-form.tsx
@@ -1,46 +1,80 @@
 import { Tooltip } from "@bera/shared-ui";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@bera/ui/form";
 import { Input } from "@bera/ui/input";
+import { type UseFormReturn } from "react-hook-form";
 
-export default function CommunityForm() {
+export default function CommunityForm({ form }: { form: UseFormReturn }) {
   return (
-    <div className="flex w-full flex-col justify-start gap-8">
-      <div className="inline-flex flex-col justify-start gap-2">
-        <div className="text-sm font-semibold leading-tight">
-          Recipient <Tooltip text="test" />
-        </div>
-        <Input
-          type="text"
-          id="forum-discussion-link"
-          placeholder="0x0000...0000"
-          // value={"forum-discussion-link"}
-          // onChange={(e) => {}}
-        />
-      </div>
+    <>
+      <FormField
+        control={form.control}
+        name="recipient"
+        render={({ field }) => (
+          <FormItem className="inline-flex flex-col justify-start gap-2">
+            <div className="text-sm font-semibold leading-tight">
+              Recipient <Tooltip text="test" />
+            </div>
+            <div>
+              <FormControl>
+                <Input
+                  type="text"
+                  id="forum-discussion-link"
+                  placeholder="0x0000...0000"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage className="mt-2" />
+            </div>
+          </FormItem>
+        )}
+      />
 
-      <div className="inline-flex flex-col justify-start gap-2">
-        <div className="text-sm font-semibold leading-tight">Amount</div>
-        <Input
-          type="number"
-          id="forum-discussion-link"
-          placeholder="0.0"
-          // value={"forum-discussion-link"}
-          // onChange={(e) => {}}
-        />
-      </div>
+      <FormField
+        control={form.control}
+        name="amountA"
+        render={({ field }) => (
+          <FormItem className="inline-flex flex-col justify-start gap-2">
+            <div className="text-sm font-semibold leading-tight">Amount</div>
+            <div>
+              <Input
+                type="number"
+                id="forum-discussion-link"
+                placeholder="0.0"
+                {...field}
+              />
+              <FormMessage className="mt-2" />
+            </div>
+          </FormItem>
+        )}
+      />
 
-      <div className="inline-flex flex-col justify-start gap-2">
-        <div className="text-sm font-semibold leading-tight">
-          Amount <Tooltip text="test" />
-        </div>
-        <Input
-          type="number"
-          id="forum-discussion-link"
-          placeholder="0.0"
-          endAdornment="BGT"
-          // value={"forum-discussion-link"}
-          // onChange={(e) => {}}
-        />
-      </div>
-    </div>
+      <FormField
+        control={form.control}
+        name="amountB"
+        render={({ field }) => (
+          <FormItem className="inline-flex flex-col justify-start gap-2">
+            <div className="text-sm font-semibold leading-tight">
+              Amount <Tooltip text="test" />
+            </div>
+            <div>
+              <Input
+                type="number"
+                id="forum-discussion-link"
+                placeholder="0.0"
+                endAdornment="BGT"
+                {...field}
+              />
+              <FormMessage className="mt-2" />
+            </div>
+          </FormItem>
+        )}
+      />
+    </>
   );
 }
--- apps/bgt-station/src/app/governance/types.ts
+++ apps/bgt-station/src/app/governance/types.ts
@@ -27,33 +27,50 @@ const parameterChangeLineSchema = z.object({
 });
 
 export const BaseFormSchema = z.object({
-  // type: z.enum(Object.values(ProposalTypeEnum)),
+  type: z.enum(Object.values(ProposalTypeEnum)),
   title: z.string().nonempty("Required"),
-  // forumLink: z.string().url("Forum link must be a valid URL."),
-  // description: z.string(),
-  // expedite: z.boolean(),
-  // initialDeposit: z.number().gt(0, "Initial deposit must be greater than 0."),
+  forumLink: z
+    .string()
+    .nonempty("Required")
+    .url("Forum link must be a valid URL."),
+  description: z.string().nonempty("Required").min(10, {
+    message: "Description must be at least 10 characters.",
+  }),
+  expedite: z.boolean(),
+  initialDeposit: z
+    .string()
+    .nonempty("Required")
+    .refine((val) => Number(val) > 0, {
+      message: "Initial deposit must be greater than 0.",
+    }),
 });
 
-export const CommunityFormSchema = z.object({
-  ...BaseFormSchema,
-  // type: z.literal(ProposalTypeEnum.COMMUNITY_POOL_SPEND),
-  // recipient: z.string().refine((value) => isAddress(value), {
-  //   message: "Invalid recipient address.",
-  // }),
-  // amountA: z.number().gt(0, "Amount must be greater than 0."),
-  // amountB: z.number().gt(0, "Amount must be greater than 0."),
+export const CommunityFormSchema = BaseFormSchema.extend({
+  type: z.literal(ProposalTypeEnum.COMMUNITY_POOL_SPEND),
+  recipient: z.string().refine((value) => isAddress(value), {
+    message: "Invalid recipient address.",
+  }),
+  amountA: z
+    .string()
+    .nonempty("Required")
+    .refine((val) => Number(val) > 0, {
+      message: "Amount must be greater than 0.",
+    }),
+  amountB: z
+    .string()
+    .nonempty("Required")
+    .refine((val) => Number(val) > 0, {
+      message: "Amount must be greater than 0.",
+    }),
 });
 
-export const ParameterFormSchema = z.object({
-  ...BaseFormSchema,
-  // type: z.literal(ProposalTypeEnum.PARAMETER_CHANGE),
+export const ParameterFormSchema = BaseFormSchema.extend({
+  type: z.literal(ProposalTypeEnum.PARAMETER_CHANGE),
   // parameters2Change: z.array(parameterChangeLineSchema),
 });
 
-export const ExecuteFormSchema = z.object({
-  ...BaseFormSchema,
-  // type: z.literal(ProposalTypeEnum.EXECUTE_CONTRACT),
+export const ExecuteFormSchema = BaseFormSchema.extend({
+  type: z.literal(ProposalTypeEnum.EXECUTE_CONTRACT),
   // runAs: z.string().refine((value) => isAddress(value), {
   //   message: "Invalid address.",
   // }),
