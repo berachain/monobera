5d3d0b8a7076de68888f97f8ebe9268d550c2b94
System Administrator <root@ninas-MacBook-Pro.local>
2023-08-08 23:37:38 -0400

    form ui and validation

--- apps/bgt-station/src/app/governance/create/parameter-change-form.tsx
+++ apps/bgt-station/src/app/governance/create/parameter-change-form.tsx
@@ -1,21 +1,29 @@
-import { Tooltip } from "@bera/shared-ui";
-import { Input } from "@bera/ui/input";
+import { CustomParameterGroup, Tooltip } from "@bera/shared-ui";
+import { FormField, FormItem, FormMessage } from "@bera/ui/form";
+import { type UseFormReturn } from "react-hook-form";
 
-export default function ParameterForm() {
+export default function ParameterForm({ form }: { form: UseFormReturn }) {
   return (
-    <div className="flex w-full flex-col justify-start gap-8">
-      <div className="inline-flex flex-col justify-start gap-2">
-        <div className="text-sm font-semibold leading-tight">
-          Parameters to change <Tooltip text="test" />
-        </div>
-        <Input
-          type="text"
-          id="forum-discussion-link"
-          placeholder="Paste link here"
-          // value={"forum-discussion-link"}
-          // onChange={(e) => {}}
-        />
-      </div>
-    </div>
+    <>
+      <FormField
+        control={form.control}
+        name="parameters2Change"
+        render={({ field }) => (
+          <FormItem className="inline-flex flex-col justify-start gap-2">
+            <div className="text-sm font-semibold leading-tight">
+              Parameters to change <Tooltip text="test" />
+            </div>
+            <div>
+              <CustomParameterGroup
+                onSubmit={(parameterFrom) =>
+                  form.setValue("parameters2Change", parameterFrom)
+                }
+              />
+              <FormMessage className="mt-[-24px]" />
+            </div>
+          </FormItem>
+        )}
+      />
+    </>
   );
 }
