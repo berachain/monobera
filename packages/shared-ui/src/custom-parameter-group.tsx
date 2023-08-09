2a442fac4a0de8328c4be96db3bd1163342ec467
System Administrator <root@ninas-MacBook-Pro.local>
2023-08-09 12:02:51 -0400

    comments

--- packages/shared-ui/src/custom-parameter-group.tsx
+++ packages/shared-ui/src/custom-parameter-group.tsx
@@ -14,19 +14,19 @@ export const CustomParameterGroup = ({
   onSubmit: (parameterFrom: ParameterChangeLine[]) => void;
 }) => {
   const newLine = { subspace: "", key: "", value: "" };
-  const [parameterFrom, setParameterForm] = useState<ParameterChangeLine[]>([
+  const [parameterForm, setParameterForm] = useState<ParameterChangeLine[]>([
     newLine,
   ]);
   useEffect(() => {
     onSubmit(
-      parameterFrom.filter(
+      parameterForm.filter(
         (param) => !(!param.subspace && !param.key && !param.value),
       ),
     );
-  }, [parameterFrom]);
+  }, [parameterForm]);
   return (
     <div>
-      {parameterFrom.map((param, index) => (
+      {parameterForm.map((param, index) => (
         <div
           key={`${index}-param-group`}
           className="flex w-full items-center justify-center space-x-2 pb-4"
@@ -37,8 +37,8 @@ export const CustomParameterGroup = ({
             placeholder="Subspace"
             value={param.subspace}
             onChange={(e) => {
-              parameterFrom[index]!.subspace = e.target.value;
-              setParameterForm([...parameterFrom]);
+              parameterForm[index]!.subspace = e.target.value;
+              setParameterForm([...parameterForm]);
             }}
           />
           <Input
@@ -47,8 +47,8 @@ export const CustomParameterGroup = ({
             placeholder="Key"
             value={param.key}
             onChange={(e) => {
-              parameterFrom[index]!.key = e.target.value;
-              setParameterForm([...parameterFrom]);
+              parameterForm[index]!.key = e.target.value;
+              setParameterForm([...parameterForm]);
             }}
           />
           <Input
@@ -57,16 +57,16 @@ export const CustomParameterGroup = ({
             placeholder="Value"
             value={param.value}
             onChange={(e) => {
-              parameterFrom[index]!.value = e.target.value;
-              setParameterForm([...parameterFrom]);
+              parameterForm[index]!.value = e.target.value;
+              setParameterForm([...parameterForm]);
             }}
           />
           <div className="border-10 rounded-md border-red-300 bg-muted text-secondary hover:cursor-pointer">
             <Icons.minus
               className="h-7 w-7 rounded-xl text-muted-foreground"
               onClick={() => {
-                parameterFrom.splice(index, 1);
-                setParameterForm([...parameterFrom]);
+                parameterForm.splice(index, 1);
+                setParameterForm([...parameterForm]);
               }}
             />
           </div>
@@ -77,7 +77,7 @@ export const CustomParameterGroup = ({
         <div
           className="absolute right-0 top-0 flex items-center gap-1 text-xs text-muted-foreground hover:cursor-pointer"
           onClick={() => {
-            setParameterForm([...parameterFrom, newLine]);
+            setParameterForm([...parameterForm, newLine]);
           }}
         >
           <Icons.plusCircle className="h-3 w-3 text-muted-foreground" />
