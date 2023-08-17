0bb13f35ab1b9dd14a04ac71f92b53e6e181b286
nina yang <nina@berachain.com>
2023-08-17 13:57:07 -0400

    honey banner

--- apps/lend/src/app/page.tsx
+++ apps/lend/src/app/page.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 
 import { Header } from "~/components/header";
+import HoneyBanner from "~/components/honey-banner";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <br />
       <div className="container pt-14">
         Earn Interest And Rewards By Supplying Your Assets And Borrowing Honey
+        <HoneyBanner />
       </div>
     </>
   );
