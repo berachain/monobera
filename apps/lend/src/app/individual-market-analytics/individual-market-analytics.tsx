bc6d489940fa3ae3c827b313e383f7f448610fcf
nina yang <nina@berachain.com>
2023-08-18 10:26:34 -0400

    faster faster>:c

--- apps/lend/src/app/individual-market-analytics/individual-market-analytics.tsx
+++ apps/lend/src/app/individual-market-analytics/individual-market-analytics.tsx
@@ -1,3 +1,44 @@
-export default function IndividualMarketAnalytics() {
-  return <div>individualMarketAnalytics</div>;
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { Icons } from "@bera/ui/icons";
+import { isAddress } from "viem";
+
+import TokenInfoCard from "./components/token-info-card";
+import UserInfo from "./components/user-info";
+
+export default function IndividualMarketAnalytics({
+  address,
+}: {
+  address: `0x${string}`;
+}) {
+  const router = useRouter();
+  useEffect(() => {
+    if (!address || !isAddress(address)) {
+      // router.push("/404");
+      console.log("404");
+    }
+  }, []);
+
+  return (
+    <div>
+      <div className="mb-4 inline-flex h-10 items-center justify-center gap-2 rounded-lg border border-border px-4 py-2 hover:cursor-pointer">
+        <Icons.arrowLeft className="relative h-4 w-4" />
+        <div
+          className="text-sm font-medium leading-normal"
+          onClick={() => router.push("/")}
+        >
+          Go Back
+        </div>
+      </div>
+
+      <TokenInfoCard />
+
+      <div className="flex flex-col lg:flex-row">
+        <UserInfo />
+        <UserInfo />
+      </div>
+    </div>
+  );
 }
--- apps/lend/src/app/individual-market-analytics/page.tsx
+++ apps/lend/src/app/individual-market-analytics/page.tsx
@@ -2,6 +2,12 @@ import React from "react";
 
 import IndividualMarketAnalytics from "./individual-market-analytics";
 
-export default function Page() {
-  return <IndividualMarketAnalytics />;
+export default function Page({
+  searchParams,
+}: {
+  searchParams: {
+    address: `0x${string}`;
+  };
+}) {
+  return <IndividualMarketAnalytics address={searchParams.address} />;
 }
