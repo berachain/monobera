86d0893bc9403891f89c64fc0bc2541ea1790075
nina yang <nina@berachain.com>
2023-08-30 12:58:43 -0400

    fix build

--- packages/bera-router/src/services/index.ts
+++ packages/bera-router/src/services/index.ts
@@ -1,4 +1,12 @@
 export * from "./RouterService/routerService";
 export * from "./RouterService/types";
 export * from "./RouterService/errors";
-export * from "./PoolService/types";
+export type {
+  TokenData,
+  PoolRecord,
+  Pool,
+  PoolRecords,
+  PoolData,
+  WeightEntry,
+  RawPool,
+} from "./PoolService/types";
