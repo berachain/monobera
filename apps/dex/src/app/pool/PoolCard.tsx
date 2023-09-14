b56ae57422b826a07233164101b7872bddc527d1
nina yang <nina@berachain.com>
2023-09-13 21:41:08 -0400

    loading

--- apps/dex/src/app/pool/PoolCard.tsx
+++ apps/dex/src/app/pool/PoolCard.tsx
@@ -27,7 +27,7 @@ export const PoolCard = ({ pool }: { pool: Pool | undefined }) => {
       key={pool?.pool}
       className="col-span-1 flex w-full max-w-[275px] flex-col justify-end rounded-xl border border-border bg-background"
     >
-      <div className="flex flex-col items-center justify-center gap-1 px-6 pb-4 pt-10">
+      <div className="flex flex-col items-center justify-center gap-1 p-6 pb-4">
         <TokenIconList
           tokenList={pool?.tokens?.map((t) => t.address) ?? []}
           size="lg"
@@ -76,21 +76,20 @@ export const PoolCard = ({ pool }: { pool: Pool | undefined }) => {
 
 export const PoolCardLoading = () => {
   return (
-    <div className="col-span-1 flex w-full max-w-[275px] flex-col gap-8 rounded-xl bg-muted p-8">
-      <div className="flex gap-2">
-        <Skeleton className="h-8 w-8 flex-shrink-0 rounded-full" />
-        <Skeleton className="h-8 w-full" />
+    <div className="col-span-1 flex w-full max-w-[275px] flex-col rounded-xl bg-background">
+      <div className="flex flex-col items-center gap-1 px-6 pb-4 pt-6">
+        <Skeleton className="h-6 w-1/2" />
+        <Skeleton className="h-12 w-full" />
+        <Skeleton className="h-7 w-full" />
       </div>
-      <Skeleton className="h-8 w-full" />
-      <div className="grid grid-cols-2 gap-2">
-        {[0, 0, 0, 0].map((_, index) => (
-          <Skeleton className="h-16 w-full" key={index} />
-        ))}
+      <div className="bg-muted px-6 py-3 gap-1 flex flex-col">
+        <Skeleton className="h-6 w-full" />
+        <Skeleton className="h-6 w-full" />
+        <Skeleton className="h-6 w-full" />
       </div>
-
-      <div className="flex h-8 gap-2">
-        <Skeleton className="h-full w-8 flex-shrink-0" />
-        <Skeleton className="h-full w-full" />
+      <div className="flex w-full gap-1 bg-muted p-3 pt-6 ">
+        <Skeleton className="h-[44px] w-14 flex-shrink-0" />
+        <Skeleton className="h-[44px] w-full" />
       </div>
     </div>
   );
--- apps/dex/src/components/tag-list.tsx
+++ apps/dex/src/components/tag-list.tsx
@@ -11,12 +11,12 @@ export function TagList({
   className?: string;
 }) {
   return (
-    <div className={cn("ml-2 flex h-12 w-fit items-center", className)}>
+    <div className={cn("ml-2 flex w-fit items-center", className)}>
       {tagList.map((tag) => (
         <Badge
           key={tag}
           variant={"info"}
-          className="-ml-2 flex h-7 w-7 items-center justify-center rounded-full p-0 text-xs"
+          className="-ml-2 flex h-6 w-6 items-center justify-center rounded-full p-0 pt-1 pr-1/2 text-[10px]"
         >
           {getBadgeContent(tag)}
         </Badge>
