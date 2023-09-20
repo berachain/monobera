84e042c64de9861e92aef586f1f9ab0456ea9d87
nina yang <nina@berachain.com>
2023-09-20 01:19:34 -0400

    general info banner

--- apps/perp/src/app/berpetuals/page.tsx
+++ apps/perp/src/app/berpetuals/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { InstrumentDropdown } from "./components/instrument-dropdown";
+import { GeneralInfoBanner } from "./components/general-info-banner";
 import PanelLeft from "./panel-left";
 
 export default function Home() {
@@ -10,7 +11,7 @@ export default function Home() {
         <div className="w-full flex-shrink-0 flex-grow-0 lg:w-[400px]">
           <InstrumentDropdown />
         </div>
-        <div className="h-[65px] w-full border-b border-border">Total Info</div>
+        <GeneralInfoBanner/>
       </div>
       <div className="flex w-full">
         <PanelLeft />
