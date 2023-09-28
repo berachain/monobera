1c8665a1f1cf736205a2a280c015c2f785c0353e
Garrett Parris <garrettp18@gmail.com>
2023-09-28 16:16:53 -0400

    log

--- apps/bgt-station/src/app/Providers.tsx
+++ apps/bgt-station/src/app/Providers.tsx
@@ -7,6 +7,7 @@ import { ThemeProvider } from "~/components/theme-provider";
 import { beraJsConfig } from "./config";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
+  console.log('I AM HERE WHYTYYTER')
   return (
     <BeraConfig autoConnect={true} networkConfig={beraJsConfig}>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
