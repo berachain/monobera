be5d3cdf50ca39fd99654852a4d2674b13b69867
Garrett Parris <garrettp18@gmail.com>
2023-10-08 23:35:16 -0400

    server side price mapping

--- apps/dex/src/hooks/usePollSwaps.tsx
+++ apps/dex/src/hooks/usePollSwaps.tsx
@@ -1,7 +1,6 @@
 import { type BatchSwapStep } from "@bera/bera-router";
 import { POLLING } from "@bera/shared-ui/src/utils";
 import useSWR from "swr";
-import { formatUnits } from "viem";
 import { type Address } from "wagmi";
 
 import { getSwap } from "~/app/api/getPrices/api/getPrices";
@@ -32,35 +31,7 @@ export const usePollSwaps = ({
     async () => {
       try {
         const result = await getSwap(tokenIn, tokenOut, swapKind, amount);
-
-        if (!result.steps)
-          return {
-            batchSwapSteps: [],
-            formattedSwapAmount: amount.toString(),
-            formattedReturnAmount: "0",
-          };
-
-        const batchSwapSteps: BatchSwapStep[] = result.steps.map(
-          (step: any) => {
-            return {
-              poolId: step.pool,
-              assetIn: step.assetIn,
-              amountIn: BigInt(step.amountIn),
-              assetOut: step.assetOut,
-              amountOut: step.amountOut,
-              userData: "",
-            };
-          },
-        );
-        const swapInfo = {
-          batchSwapSteps: batchSwapSteps,
-          formattedSwapAmount: amount.toString(),
-          formattedReturnAmount: formatUnits(
-            BigInt(result.steps[result.steps.length - 1].amountOut),
-            18,
-          ),
-        };
-        return swapInfo;
+        return result;
       } catch (e) {
         // console.log(e);
         // TODO: throws so many errors but this is good 4 debug
