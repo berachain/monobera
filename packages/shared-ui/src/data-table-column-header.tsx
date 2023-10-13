16580730e51daab7bcc4d6e57fed43a1b4af0482
nina yang <nina@berachain.com>
2023-10-13 15:45:07 -0400

    table header refactor

--- packages/shared-ui/src/data-table-column-header.tsx
+++ packages/shared-ui/src/data-table-column-header.tsx
@@ -1,11 +1,5 @@
 import { cn } from "@bera/ui";
 import { Button } from "@bera/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@bera/ui/dropdown-menu";
 import { Icons } from "@bera/ui/icons";
 import { type Column } from "@tanstack/react-table";
 
@@ -35,26 +29,35 @@ export function DataTableColumnHeader<TData, TValue>({
 
   return (
     <div className={cn("flex w-full items-center space-x-2", className)}>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button
-            variant="ghost"
-            size="sm"
-            className="-ml-3 h-8 text-sm hover:bg-hover data-[state=open]:bg-hover"
-          >
-            <span>
-              {title}
-              {tooltip && <Tooltip text={tooltip} />}
-            </span>
-            {column.getIsSorted() === "desc" ? (
-              <Icons.sortDesc className="ml-2 h-4 w-4" />
-            ) : column.getIsSorted() === "asc" ? (
-              <Icons.sortAsc className="ml-2 h-4 w-4" />
-            ) : (
-              <Icons.arrowDownUp className="ml-2 h-4 w-4" />
-            )}
-          </Button>
-        </DropdownMenuTrigger>
+      {/* <DropdownMenu>
+        <DropdownMenuTrigger asChild> */}
+      <Button
+        variant="ghost"
+        size="sm"
+        className="-ml-3 h-8 text-sm hover:bg-hover data-[state=open]:bg-hover"
+        onClick={() => {
+          if (column.getIsSorted() === "desc") {
+            column.toggleSorting(false);
+          } else if (column.getIsSorted() === "asc") {
+            column.toggleSorting(true);
+          } else {
+            column.toggleSorting(true);
+          }
+        }}
+      >
+        <span>
+          {title}
+          {tooltip && <Tooltip text={tooltip} />}
+        </span>
+        {column.getIsSorted() === "desc" ? (
+          <Icons.sortDesc className="ml-2 h-4 w-4 text-accent" />
+        ) : column.getIsSorted() === "asc" ? (
+          <Icons.sortAsc className="ml-2 h-4 w-4 text-accent" />
+        ) : (
+          <Icons.arrowDownUp className="ml-2 h-4 w-4" />
+        )}
+      </Button>
+      {/* </DropdownMenuTrigger>
         <DropdownMenuContent align="start">
           <DropdownMenuItem onClick={() => column.toggleSorting(false)}>
             <Icons.sortAsc className="mr-2 h-3.5 w-3.5 text-muted-foreground/70" />
@@ -65,7 +68,7 @@ export function DataTableColumnHeader<TData, TValue>({
             Desc
           </DropdownMenuItem>
         </DropdownMenuContent>
-      </DropdownMenu>
+      </DropdownMenu> */}
     </div>
   );
 }
