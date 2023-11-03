097e3d0370cf2d414b91a80a5012bf3a4a68322c
Garrett Parris <garrettp18@gmail.com>
2023-11-03 10:38:24 -0400

    add bera swap

--- apps/dex/src/components/swap-card.tsx
+++ apps/dex/src/components/swap-card.tsx
@@ -100,7 +100,6 @@ export function SwapCard({
     outputCurrency,
   });
 
-  console.log("swapInfo", swapInfo);
   const { isConnected } = useBeraJs();
   const [exceedingBalance, setExceedingBalance] = useState(false);
 
