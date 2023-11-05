08b2467f024b063a6feb79fbf7d738f1ddbacbca
NINI BEAR <141682447+ninibear030@users.noreply.github.com>
2023-11-05 13:34:56 -0500

    Bgt wiping walrus ass (#206)
    * bgt improve
    
    * sm fix
    
    * small fix

--- apps/bgt-station/src/app/layout.tsx
+++ apps/bgt-station/src/app/layout.tsx
@@ -2,9 +2,7 @@
 
 import "@bera/ui/styles.css";
 import "../styles/globals.css";
-import dynamic from "next/dynamic";
 import { IBM_Plex_Sans } from "next/font/google";
-import { cloudinaryUrl } from "@bera/config";
 import { Footer, Header, TailwindIndicator } from "@bera/shared-ui";
 import { cn } from "@bera/ui";
 import { Analytics } from "@vercel/analytics/react";
@@ -18,22 +16,6 @@ const fontSans = IBM_Plex_Sans({
   variable: "--font-sans",
   subsets: ["latin"],
 });
-
-const Gradient = dynamic(
-  () => import("@bera/shared-ui").then((mod) => mod.Gradient),
-  {
-    ssr: false,
-    loading: () => <></>,
-  },
-);
-
-const BearBG = dynamic(
-  () => import("@bera/shared-ui").then((mod) => mod.BearBG),
-  {
-    ssr: false,
-    loading: () => <></>,
-  },
-);
 
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
@@ -53,14 +35,6 @@ export default function RootLayout(props: { children: React.ReactNode }) {
               </main>
               <Footer />
             </div>
-            <Gradient
-              lightUrl={`${cloudinaryUrl}/shared/xrvkmr8yhvvyckxznty2`}
-              darkUrl={`${cloudinaryUrl}/shared/klszfo1j2sz9yk7lin87`}
-            />
-            <BearBG
-              lightUrl={`${cloudinaryUrl}/shared/qfyewc7lo2ujtktbimd8`}
-              darkUrl={`${cloudinaryUrl}/shared/banner-bears-dark`}
-            />
           </div>
           <TailwindIndicator />
           <Analytics />
