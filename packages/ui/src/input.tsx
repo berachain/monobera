82d965285102357c7ade2fbf654349aa9c0acf38
Garrett Parris <garrettp18@gmail.com>
2023-11-20 18:55:47 -0500

    11 17 feedback (#249) (#253)
    * 11 17 feedback (#249)
    
    * 11 16 UI change (#247)
    
    * 11 17 feedback (#250)
    
    * faucet fix and button fix (#252)
    
    Co-authored-by: NINI BEAR <141682447+ninibear030@users.noreply.github.com>
    Co-authored-by: Garrett Parris <garrettp18@gmail.com>
    
    ---------
    
    Signed-off-by: NINI BEAR <141682447+ninibear030@users.noreply.github.com>
    Co-authored-by: NINI BEAR <141682447+ninibear030@users.noreply.github.com>

--- packages/ui/src/input.tsx
+++ packages/ui/src/input.tsx
@@ -24,7 +24,7 @@ const Input = React.forwardRef<HTMLInputElement, CustomInputProps>(
         <input
           type={type}
           className={cn(
-            "focus:border-1 flex h-10 w-full rounded-xl border border-border px-3 py-2 text-sm text-foreground ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus:border-foreground focus:outline-none disabled:cursor-not-allowed disabled:text-muted-foreground",
+            "focus:border-1 flex h-10 w-full rounded-md border border-border px-3 py-2 text-sm text-foreground ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus:border-foreground focus:outline-none disabled:cursor-not-allowed disabled:text-muted-foreground",
             className,
           )}
           ref={ref}
