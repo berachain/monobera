d41e9bb84aebff55cc0e12cf0d6e904aa0d9894b
nina yang <bina@berachain.com>
2023-12-08 11:19:31 -0500

    approve

--- packages/shared-ui/src/approve-button.tsx
+++ packages/shared-ui/src/approve-button.tsx
@@ -2,6 +2,7 @@
 
 import { TransactionActionType, type Token } from "@bera/berajs";
 import { Button } from "@bera/ui/button";
+import { maxInt256 } from "viem";
 import { erc20ABI } from "wagmi";
 
 import { useTxn } from "./hooks/useTxn";
@@ -12,30 +13,44 @@ type Props = {
   amount?: bigint;
 };
 
-export const ApproveButton = ({
-  token,
-  spender,
-  amount = 1000000000000000000n,
-}: Props) => {
+export const ApproveButton = ({ token, spender, amount }: Props) => {
   const { write, isLoading, isSubmitting } = useTxn({
     message: `Approve ${token?.name}`,
     actionType: TransactionActionType.APPROVAL,
   });
 
   return (
-    <Button
-      className="w-full"
-      disabled={!token || isLoading || isSubmitting}
-      onClick={() => {
-        write({
-          address: token?.address as `0x${string}`,
-          abi: erc20ABI as unknown as (typeof erc20ABI)[],
-          functionName: "approve",
-          params: [spender, amount],
-        });
-      }}
-    >
-      {isLoading ? "Loading..." : `Approve ${token?.symbol}`}
-    </Button>
+    <div className="flex gap-4">
+      {amount !== undefined && (
+        <Button
+          className="w-full"
+          disabled={!token || isLoading || isSubmitting}
+          onClick={() => {
+            write({
+              address: token?.address as `0x${string}`,
+              abi: erc20ABI as unknown as (typeof erc20ABI)[],
+              functionName: "approve",
+              params: [spender, amount],
+            });
+          }}
+        >
+          {isLoading ? "Loading..." : `Approve ${token?.symbol}`}
+        </Button>
+      )}
+      <Button
+        className="w-full"
+        disabled={!token || isLoading || isSubmitting}
+        onClick={() => {
+          write({
+            address: token?.address as `0x${string}`,
+            abi: erc20ABI as unknown as (typeof erc20ABI)[],
+            functionName: "approve",
+            params: [spender, maxInt256],
+          });
+        }}
+      >
+        {isLoading ? "Loading..." : `Approve Infinite`}
+      </Button>
+    </div>
   );
 };
