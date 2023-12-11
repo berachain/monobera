41dfa6076889fb5a6bb196df3a67151ed130496c
nina yang <bina@berachain.com>
2023-12-11 16:09:42 -0500

    inital

---
+++ apps/validator-clue/src/app/layout.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import "@bera/ui/styles.css";
+import "../styles/globals.css";
+import { IBM_Plex_Sans } from "next/font/google";
+import Script from "next/script";
+import { Header, TailwindIndicator } from "@bera/shared-ui";
+import { cn } from "@bera/ui";
+import { Analytics } from "@vercel/analytics/react";
+import { Toaster } from "react-hot-toast";
+
+import { Footer } from "~/components/footer";
+import Providers from "./Providers";
+
+const fontSans = IBM_Plex_Sans({
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-sans",
+  subsets: ["latin"],
+});
+
+export default function RootLayout(props: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <Script
+        id="HotJarAnalytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:3728319,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+        }}
+      />
+      <body
+        className={cn("bg-background font-sans antialiased", fontSans.variable)}
+      >
+        <Providers>
+          <div className="z-[100]">
+            <Toaster position="bottom-right" />
+          </div>
+          <div className="relative flex min-h-screen w-full flex-col overflow-hidden bg-background">
+            <Header navItems={[]} hideTheme />
+            <main className="mt-[72px] min-h-[calc(100vh-72px)] w-full bg-sky-600 pb-[70px]">
+              {props.children}
+            </main>
+            <Footer />
+          </div>
+          <TailwindIndicator />
+          <Analytics />
+        </Providers>
+      </body>
+    </html>
+  );
+}
---
+++ apps/validator-clue/src/app/page.tsx
@@ -0,0 +1,30 @@
+import { type Metadata } from "next";
+import Image from "next/image";
+import { cloudinaryUrl, faucetName } from "@bera/config";
+
+import { Explore } from "~/components/explore";
+import Content from "./content";
+
+export const metadata: Metadata = {
+  title: `Berachain ${faucetName} `,
+  description: `Fund your testnet wallet with bera tokens.`,
+};
+
+export default function Page() {
+  return (
+    <div className="container mx-auto">
+      <div className="flex w-full flex-col-reverse items-center justify-between py-12 xl:flex-row ">
+        <Content />
+        <Image
+          src={`${cloudinaryUrl}/faucet/faucet_v3_uktibg`}
+          alt="machine"
+          width={523}
+          height={614}
+          loading="eager"
+          className="hidden h-[614px] object-cover xl:block"
+        />
+      </div>
+      <Explore />
+    </div>
+  );
+}
