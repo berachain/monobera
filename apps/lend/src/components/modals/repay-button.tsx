cbc068cc8981b56e6e2bece5ad904c24eae620f7
Coin Bera <coin@berachain.com>
2024-01-08 16:35:00 -0500

    more-tooltips-bend

--- apps/lend/src/components/modals/repay-button.tsx
+++ apps/lend/src/components/modals/repay-button.tsx
@@ -120,7 +120,7 @@ const RepayModalContent = ({
   });
 
   return (
-    <div className="flex flex-col gap-6">
+    <div className="flex flex-col gap-6 pb-4">
       <div className="text-lg font-semibold leading-7">Repay</div>
       <div className="rounded-md border border-border bg-input">
         <TokenInput
