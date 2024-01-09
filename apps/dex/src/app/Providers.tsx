9a5fb619dd56c85e6e00a76290d76de857a54987
Garrett Parris <garrettp18@gmail.com>
2024-01-08 21:10:43 -0500

    bump

--- apps/dex/src/app/Providers.tsx
+++ apps/dex/src/app/Providers.tsx
@@ -7,6 +7,7 @@ import { ThemeProvider } from "~/components/theme-provider";
 import { beraJsConfig } from "./config";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
+  
   return (
     <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
       <BeraConfig autoConnect={true} networkConfig={beraJsConfig}>
