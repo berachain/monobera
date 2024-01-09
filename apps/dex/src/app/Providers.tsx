2288ae1c7305f11521af6d4e96ebe8ba441b9181
Garrett Parris <garrettp18@gmail.com>
2024-01-08 20:52:34 -0500

    remove nextauth

--- apps/dex/src/app/Providers.tsx
+++ apps/dex/src/app/Providers.tsx
@@ -2,19 +2,16 @@
 
 import React from "react";
 import { BeraConfig } from "@bera/berajs";
-import { SessionProvider } from "next-auth/react";
 
 import { ThemeProvider } from "~/components/theme-provider";
 import { beraJsConfig } from "./config";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <SessionProvider>
-      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-        <BeraConfig autoConnect={true} networkConfig={beraJsConfig}>
-          {children}
-        </BeraConfig>
-      </ThemeProvider>
-    </SessionProvider>
+    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+      <BeraConfig autoConnect={true} networkConfig={beraJsConfig}>
+        {children}
+      </BeraConfig>
+    </ThemeProvider>
   );
 }
