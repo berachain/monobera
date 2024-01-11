70b7ac33c1bc4ea2a24406b3724077ef84e381a8
NINI BEAR <141682447+ninibear030@users.noreply.github.com>
2024-01-11 17:09:28 -0500

    [wip]captcha (#354)

--- apps/faucet/src/app/layout.tsx
+++ apps/faucet/src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout(props: { children: React.ReactNode }) {
   );
   return (
     <html lang="en">
+      <Script src={`https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}`}/>
       <Script
         id="HotJarAnalytics"
         strategy="afterInteractive"
---
+++ apps/faucet/src/app/recaptcha-btn.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Button } from "@bera/ui/button";
+
+const ReCAPTCHAButton = ({
+  setToken,
+  bot,
+  setBot,
+}: {
+  setToken: (token:string) => void;
+  bot: boolean | undefined;
+  setBot: (bot: boolean) => void;
+}) => {
+  const onClick = (e:any) => {
+    e.preventDefault();
+     //@ts-ignore
+    if (typeof window.grecaptcha !== "undefined") {
+      //@ts-ignore
+      window.grecaptcha.ready(() => {
+        //@ts-ignore
+        window.grecaptcha
+          .execute(process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY, {
+            action: "submit",
+          })
+          .then((token:any) => {
+            if (token) {
+              setToken(token);
+              setBot(false);
+            } else {
+              setBot(true);
+            }
+          });
+      });
+    }
+  };
+
+  return (
+    <Button
+      onClick={onClick}
+      variant={bot ? "destructive" : "primary"}
+      disabled={bot}
+      className="w-full"
+    >
+      {bot ? "Your sus" : "Click here to prove you are not a bot"}
+    </Button>
+  );
+};
+
+export default ReCAPTCHAButton;
