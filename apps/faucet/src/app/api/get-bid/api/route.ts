090e418fc67c33722e25d61b725a814c97dcc4dc
nina yang <bina@berachain.com>
2024-01-15 18:04:00 -0500

    smol tings

--- apps/faucet/src/app/api/get-bid/api/route.ts
+++ apps/faucet/src/app/api/get-bid/api/route.ts
@@ -1,3 +1,6 @@
+export const dynamic = 'force-dynamic'
+export const fetchCache = 'force-no-store'
+
 import { NextResponse } from "next/server";
 import { faucetEndpointUrl } from "@bera/config";
 
