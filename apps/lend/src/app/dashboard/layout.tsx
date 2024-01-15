0e18585bdac01decffd01ad3d7ac61c5a646548b
Garrett Parris <garrettp18@gmail.com>
2024-01-15 12:29:15 -0500

    apy fix and parameterize banner (#381)

--- apps/lend/src/app/dashboard/layout.tsx
+++ apps/lend/src/app/dashboard/layout.tsx
@@ -1,11 +1,13 @@
-import { lendName } from "@bera/config";
+import { bannerEnabled, lendName } from "@bera/config";
 import { LaunchBanner } from "@bera/shared-ui";
 import { FooterSM } from "@bera/shared-ui/src/footer";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <section>
-      <LaunchBanner appName={lendName} className="-mt-8 mb-8" />
+      {bannerEnabled && (
+        <LaunchBanner appName={lendName} className="-mt-8 mb-8" />
+      )}
       <div className="container min-h-minimun max-w-[1440px] pb-16">
         {children}
       </div>
