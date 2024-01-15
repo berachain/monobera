9465d2fe80a9d9b2b6826fb7a1f860c9439dfb8b
nina yang <bina@berachain.com>
2024-01-15 14:35:27 -0500

    twitter change

--- apps/faucet/src/components/twitter-jump.tsx
+++ apps/faucet/src/components/twitter-jump.tsx
@@ -9,8 +9,9 @@ export default function TwitterJump({
   disabled: boolean;
 }) {
   const url = "https://artio.faucet.berachain.com/";
-  const text = `Ooga+Booga!%0AI'm+claiming+my+testnet+tokens+for+Bearchain+Artio%0AThe+first+L1+not+backed+by+Paradigm+@berachain%20%0A%23berachain+%23artio+%23drip%20%23bid=${bid}%0A%0A`;
-  const link = `https://twitter.com/intent/tweet?text=${text}&url=${url}`;
+  const text = `HENLO+%26+OOGA+BOOGA+🐻%0A%0ASnag+your+testnet+tokens+now+%26+join+the+Bera+Brigade!%0A%0AExperience+the+forefront+of+innovation+with+@berachain%0AArtio+-+the+first+L1+NOT+backed+by+Paradigm🫵🐻⛓️%0A%0AProof+of+Drip🔑:%23BID=${bid}%0A`;
+  const link = `https://twitter.com/intent/tweet?text=${text}&url=${url}%0A%23Berachain+%23LiquidityUnchained`;
+
   return (
     <Button
       onClick={() => window.open(link, "_blank")}
