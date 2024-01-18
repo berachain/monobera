e45fa92921fccdec945d9cfa21192d106a2ae7bd
mei <mei@berachain.com>
2024-01-17 23:16:19 -0500

    make formatter take both string and number type

--- packages/berajs/src/utils/formatAmountSmall.ts
+++ packages/berajs/src/utils/formatAmountSmall.ts
@@ -1,7 +1,14 @@
-export const formatAmountSmall = (value: number, toFixedDigit: number) => {
-  if (isNaN(value)) return "0";
-  if (value < 0.01) {
-    return "< 0.01";
+export function formatAmountSmall(
+  value: number | string,
+  toFixedDigit: number,
+): string {
+  const numericValue = Number(value);
+
+  // Check if the value is not a number or is less than 0.01
+  if (isNaN(numericValue) || numericValue < 0.01) {
+    return numericValue < 0.01 ? "< 0.01" : "0";
   }
-  return Number(value).toFixed(toFixedDigit).toString();
-};
+
+  // Format the number and return it as a string
+  return numericValue.toFixed(toFixedDigit);
+}
