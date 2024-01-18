d354b9c37446974205d09eb64af4e03161d9c624
MeiTheBera <156114970+meithebear@users.noreply.github.com>
2024-01-18 15:10:38 -0500

    Bgt reward format update (#400)
    * save datadog progress
    
    * add format
    
    * governance
    
    * remove datadog
    
    * add
    
    * remove files
    
    * make formatter take both string and number type
    
    * ohno
    
    * adjust formatter
    
    * update logic
    
    * update
    
    * update and test
    
    * update bgt
    
    * format

--- apps/dex/src/app/rewards/rewards-card.tsx
+++ apps/dex/src/app/rewards/rewards-card.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Link from "next/link";
 import { type Pool } from "@bera/bera-router";
 import { formatUsd, formatter, usePollBgtRewards } from "@bera/berajs";
+import { formatAmountSmall } from "@bera/berajs/src/utils/formatAmountSmall";
 import { ApyTooltip, TokenIconList } from "@bera/shared-ui";
 import { Skeleton } from "@bera/ui/skeleton";
 
@@ -31,6 +32,8 @@ export default function RewardsCard({ pool }: { pool: Pool }) {
   const { userTotalValue, isPositionSizeLoading } = usePositionSize({
     pool: pool,
   });
+
+  const { isSmall, numericValue: formattedBgt } = formatAmountSmall(bgtRewards);
 
   return (
     <div className="flex w-full flex-col items-center justify-between gap-4 rounded-2xl border border-border bg-background p-4 md:p-6 lg:flex-row">
@@ -72,7 +75,9 @@ export default function RewardsCard({ pool }: { pool: Pool }) {
         <div className="flex items-center gap-4">
           <div className="flex min-w-[65px] flex-col gap-1">
             <div className=" text-left text-sm font-semibold leading-tight text-warning-foreground md:text-lg md:leading-7">
-              {Number(bgtRewards).toFixed(2)}
+              <p className="text-lg font-semibold text-foreground">
+                {isSmall ? `< ${formattedBgt}` : `${formattedBgt.toFixed(2)}`}
+              </p>
             </div>
             <div className="text-left text-xs font-medium leading-tight text-muted-foreground md:text-sm ">
               BGT earned
