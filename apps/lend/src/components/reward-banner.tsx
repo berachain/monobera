bb9f287335fdb1cf481bdafba9dce9e08eeb2165
Garrett Parris <garrettp18@gmail.com>
2024-01-19 22:25:11 -0500

    weeeee (#419)

--- apps/lend/src/components/reward-banner.tsx
+++ apps/lend/src/components/reward-banner.tsx
@@ -59,12 +59,12 @@ export const Banner = () => {
     actionType: TransactionActionType.CLAIMING_REWARDS,
     onSuccess: () => refetch(),
   });
-  const formattedRewards = formatEther(rewards as bigint ?? 0n);
+  const formattedRewards = formatEther((rewards as bigint) ?? 0n);
   const { isSmall, numericValue: formattedBgt } =
     formatAmountSmall(formattedRewards);
 
   return (
-    <div className="relative flex flex-colp>r-reverse items-start justify-center gap-4 rounded-lg border border-accent bg-gradient-to-b from-[#FFFCF2] to-[#FFF2D0] px-8 py-6 dark:from-[#27251F] dark:to-[#322400] lg:flex-row lg:gap-16">
+    <div className="flex-colp>r-reverse relative flex items-start justify-center gap-4 rounded-lg border border-accent bg-gradient-to-b from-[#FFFCF2] to-[#FFF2D0] px-8 py-6 dark:from-[#27251F] dark:to-[#322400] lg:flex-row lg:gap-16">
       {ModalPortal}
       <Image
         src={`${cloudinaryUrl}/bears/lendbear_npxhb3`}
@@ -106,9 +106,12 @@ export const Banner = () => {
             <TokenIcon address={bgtTokenAddress} fetch />
           </div>
           <div className="text-wrapper flex w-auto items-center align-middle text-lg font-bold">
-            {isLoadingApr
+            {/* {isLoadingApr
               ? "Loading..."
-              : `${parseFloat(String(bgtApr ?? "0")).toFixed(2)}% APY`}
+              : `${parseFloat(String(bgtApr ?? "0")).toFixed(2)}% APY`} */}
+                        {isLoadingApr
+              ? "Loading..."
+              : `${parseFloat('0').toFixed(2)}% APY`}
             <Tooltip
               text={
                 <>
