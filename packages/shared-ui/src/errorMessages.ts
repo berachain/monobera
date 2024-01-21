c93186e0bd87ff3ae5cdfc553eb5782a993f1cf3
mei <mei@berachain.com>
2024-01-21 01:45:47 -0500

    add comment

--- packages/shared-ui/src/errorMessages.ts
+++ packages/shared-ui/src/errorMessages.ts
@@ -3,6 +3,7 @@ export const GENERAL_ERROR_MESSAGE =
 export const BEAR_WITH_US_MESSAGE =
   "We're currently expanding our capacity to better serve you. Please bear with us; we'll be back shortly with improved performance!";
 export const RPC_ERROR_MESSAGHES = {
+  // TODO: add more specific error messages
   eth_gasPrice:
     "Try setting up your gas price to 100 bwei, if that doesn't resolve your issue, please report in discord with details.",
 };
