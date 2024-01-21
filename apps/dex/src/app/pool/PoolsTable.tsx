334ed3d1679278a5530ca7fb76b9e8bea66348b9
NINI BEAR <141682447+ninibear030@users.noreply.github.com>
2024-01-21 13:19:49 -0500

    My pool show closed position (#435)
    * stuff
    
    * changes

--- apps/dex/src/app/pool/PoolsTable.tsx
+++ apps/dex/src/app/pool/PoolsTable.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import Link from "next/link";
 import { DataTable, NotFoundBear } from "@bera/shared-ui";
 import { cn } from "@bera/ui";
@@ -84,6 +85,8 @@ export const PoolSearch = ({
     // setKeyword,
   } = usePoolTable();
 
+  const [allPoolShowed, setAllPoolShowed] = useState(false);
+
   return (
     <div
       className="w-full flex-col items-center justify-center"
@@ -107,8 +110,8 @@ export const PoolSearch = ({
           </TabsTrigger>
         </TabsList>
 
-        {poolType === "allPools" && (
-          <div className="flex w-full flex-col items-center justify-center gap-2 lg:flex-row lg:items-center lg:justify-between">
+        {poolType === "allPools" ? (
+          <div className="justsify-center flex w-full flex-col items-center gap-2 lg:flex-row lg:items-center lg:justify-between">
             {/* <SearchInput
             value={search}
             onChange={(e) => {
@@ -146,6 +149,13 @@ export const PoolSearch = ({
                 onClick={() => setIsList(!isList)}
               />
             </div>
+          </div>
+        ) : (
+          <div
+            className="cursor-pointer text-right text-info-foreground underline"
+            onClick={() => setAllPoolShowed(!allPoolShowed)}
+          >
+            {allPoolShowed ? "Show deposited pools" : "Show all pools"}
           </div>
         )}
 
@@ -198,7 +208,7 @@ export const PoolSearch = ({
         </TabsContent>
 
         <TabsContent value="userPools">
-          <MyPool isList />
+          <MyPool isList allPoolShowed={allPoolShowed} />
         </TabsContent>
       </Tabs>
     </div>
