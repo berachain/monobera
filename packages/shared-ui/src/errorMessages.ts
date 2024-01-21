76d1ea50fcc07f7428ad1a26c2ce635f07b4124d
mei <mei@berachain.com>
2024-01-21 09:38:10 -0500

    detailed messages

--- packages/shared-ui/src/errorMessages.ts
+++ packages/shared-ui/src/errorMessages.ts
@@ -2,7 +2,23 @@ export const GENERAL_ERROR_MESSAGE =
   "Something went wrong. Please try again later.";
 export const BEAR_WITH_US_MESSAGE =
   "We're currently expanding our capacity to better serve you. Please bear with us; we'll be back shortly with improved performance!";
-export const RPC_ERROR_MESSAGHES = {
-  eth_gasPrice:
-    "Try setting up your gas price to 100 bwei, if that doesn't resolve your issue, please report in discord with details.",
+
+export const RPC_ETH_GASPRICE_MESSAGES = {
+  method: "eth_gasPrice",
+  err: "Try setting up your gas price to 100 bwei, if that doesn't resolve your issue, please report in discord with details.",
+};
+
+export const RPC_ETH_CALL_MESSAGES = {
+  method: "eth_call",
+  err: "It seems an RPC error has occurred. Please try your request once more later.",
+};
+
+export const RPC_ETH_GETLOGS_MESSAGES = {
+  method: "eth_getLogs",
+  err: "An RPC error seems to have occurred. We advise trying your request again later.",
+};
+
+export const RPC_ETH_GETBALANCE_MESSAGES = {
+  method: "eth_getBalance",
+  err: "An RPC error has been detected. Please attempt your request after a short while.",
 };
