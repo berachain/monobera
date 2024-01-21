dfcae1b9e527eae55ebae1b3cbe538e38dde6943
NINI BEAR <141682447+ninibear030@users.noreply.github.com>
2024-01-21 12:11:18 -0500

    banner up (#434)

--- apps/dex/src/app/layout.tsx
+++ apps/dex/src/app/layout.tsx
@@ -4,6 +4,7 @@ import "@bera/ui/styles.css";
 import "../styles/globals.css";
 import { IBM_Plex_Sans } from "next/font/google";
 import Script from "next/script";
+import { rpcBannerEnabled } from "@bera/config";
 import { Header, TailwindIndicator, TermOfUseModal } from "@bera/shared-ui";
 import { cn } from "@bera/ui";
 import { Analytics } from "@vercel/analytics/react";
@@ -58,7 +59,14 @@ export default function RootLayout(props: { children: React.ReactNode }) {
               </div>
               <div className="z-10 flex-1">
                 <Header navItems={navItems} />
-                <main className="w-full pt-start">{props.children}</main>
+                <main
+                  className={cn(
+                    "w-full",
+                    rpcBannerEnabled ? "pt-start-lg" : "pt-start",
+                  )}
+                >
+                  {props.children}
+                </main>
               </div>
             </div>
             <TailwindIndicator />
--- packages/shared-ui/src/launch-banner.tsx
+++ packages/shared-ui/src/launch-banner.tsx
@@ -24,3 +24,22 @@ export const LaunchBanner = ({
     </header>
   );
 };
+
+export const RPCBanner = ({ className }: { className?: string }) => {
+  return (
+    <header
+      className={cn(
+        "top-[72px] z-50 flex h-12 w-[100vw] items-center justify-center gap-2 bg-destructive-foreground px-1 py-3 text-white",
+        className,
+      )}
+    >
+      <div className="flex items-center gap-2">
+        <Icons.clock8 className="h-4 w-4" />
+        <div className="md:text-md text-sm font-semibold leading-tight">
+          We are currently upgrading our infrastructure to meet the increased
+          demand. Please bear with us; we will be back shortly.
+        </div>
+      </div>
+    </header>
+  );
+};
