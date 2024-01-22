3101cb529a573702ac0afc31312818a1626ac7e5
MeiTheBera <156114970+meithebear@users.noreply.github.com>
2024-01-22 16:55:31 -0500

    [Ready for Review] Show different PnL values for Berp table tabs and add hover state (#438)
    * add hover state for total relative pnl
    
    * style update
    
    * make hover state a shared-ui
    
    * remove change

--- apps/perp/src/app/berpetuals/components/total-amount.tsx
+++ apps/perp/src/app/berpetuals/components/total-amount.tsx
@@ -1,17 +1,22 @@
 import { useMemo } from "react";
 import { formatUsd } from "@bera/berajs";
+import { HoverState } from "@bera/shared-ui";
 import { cn } from "@bera/ui";
 
 import { usePollOpenPositions } from "~/hooks/usePollOpenPositions";
 import { usePollTradingHistory } from "~/hooks/usePollTradingHistory";
 import type { IMarket } from "../page";
+import { type BerpTabTypes } from "./order-history-header";
+import { TotalRelativePnLHoverState } from "./total-relative-pnl-hover-state";
 
 export function TotalAmount({
   className,
   markets,
+  tabType,
 }: {
   className?: string;
   markets: IMarket[];
+  tabType: BerpTabTypes;
 }) {
   const { useTotalUnrealizedPnl, useTotalPositionSize } =
     usePollOpenPositions();
@@ -25,16 +30,45 @@ export function TotalAmount({
     return totalUnrealizedPnl + realizedPnl;
   }, [totalUnrealizedPnl, realizedPnl]);
 
-  return (
-    <div
-      className={cn(
-        "flex justify-between border-y border-border p-3",
-        className,
-      )}
-    >
+  const totalRelativePnL = () => {
+    return (
       <div className="flex flex-col items-center sm:flex-row sm:gap-2">
         <span className="text-xs font-medium text-muted-foreground">
           Total Relative Pnl
+        </span>
+        <div className="group relative">
+          <span className="font-medium text-success-foreground">
+            <span
+              className={cn(
+                "cursor-help",
+                "underline",
+                "decoration-dashed",
+                Number(totalPnl) > 0
+                  ? "text-success-foreground"
+                  : "text-destructive-foreground",
+              )}
+            >
+              {formatUsd(totalPnl)}
+            </span>
+          </span>
+
+          <HoverState>
+            <TotalRelativePnLHoverState
+              totalUnrealizedPnl={totalUnrealizedPnl}
+              realizedPnl={realizedPnl}
+              totalPnl={totalPnl}
+            />
+          </HoverState>
+        </div>
+      </div>
+    );
+  };
+
+  const totalOpenPnL = () => {
+    return (
+      <div className="flex flex-col items-center sm:flex-row sm:gap-2">
+        <span className="text-xs font-medium text-muted-foreground">
+          Total Open Pnl
         </span>
         <span className="font-medium text-success-foreground">
           <span
@@ -45,10 +79,23 @@ export function TotalAmount({
                 : "text-destructive-foreground",
             )}
           >
-            {formatUsd(totalPnl)}
+            {formatUsd(totalUnrealizedPnl)}
           </span>
         </span>
       </div>
+    );
+  };
+
+  return (
+    <div
+      className={cn(
+        "flex justify-between border-y border-border p-3",
+        className,
+      )}
+    >
+      {tabType === "positions" || tabType === "orders"
+        ? totalOpenPnL()
+        : totalRelativePnL()}
 
       <div className="flex flex-col items-center sm:flex-row sm:gap-2">
         <span className="text-xs font-medium text-muted-foreground">
