aee76024c9ae9a24c3ae1dcea4273991f19d7480
Garrett Parris <garrettp18@gmail.com>
2024-01-24 18:10:55 -0500

    Y NO WORK

--- apps/perp/src/app/vault/layout.tsx
+++ apps/perp/src/app/vault/layout.tsx
@@ -1,11 +1,12 @@
 import { bannerEnabled, perpsName } from "@bera/config";
-import { LaunchBanner, UpTimeStatus } from "@bera/shared-ui";
+import { UpTimeStatus } from "@bera/shared-ui";
+import { BerpsBanner } from "@bera/shared-ui/src/launch-banner";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <section className="relative pb-[72px] ">
       {bannerEnabled && (
-        <LaunchBanner appName={perpsName} className="mb-8 mt-0" />
+        <BerpsBanner appName={perpsName} className="mb-8 mt-0" />
       )}
       <div className="container min-h-minimun max-w-1280">{children}</div>
       <UpTimeStatus />
