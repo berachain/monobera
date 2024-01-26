db5016ebcec6fdb0e6c5d4313f6510d8755e3996
Garrett Parris <garrettp18@gmail.com>
2024-01-26 10:13:05 -0500

    Deprecate ethers v6 (#466)

--- apps/perp/src/app/berpetuals/layout.tsx
+++ apps/perp/src/app/berpetuals/layout.tsx
@@ -1,14 +1,14 @@
 "use client";
 
 import Script from "next/script";
-import { bannerEnabled} from "@bera/config";
+import { bannerEnabled } from "@bera/config";
 import { UpTimeStatus } from "@bera/shared-ui";
+import { BerpsBanner } from "@bera/shared-ui/src/launch-banner";
 
 // import { useReadLocalStorage } from "usehooks-ts";
 
 // import { Disclaimer } from "../components/disclaimer";
 import { OneClickBanner } from "./components/one-click-banner";
-import { BerpsBanner } from "@bera/shared-ui/src/launch-banner";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   // const disclaimerAccepted = useReadLocalStorage<boolean | null>(
