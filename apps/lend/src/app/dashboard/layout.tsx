ddb8697ab47d93595870baa99afaba0a4a9b57ad
Garrett Parris <garrettp18@gmail.com>
2024-01-28 22:48:12 -0500

    bullish (#526)

--- apps/lend/src/app/dashboard/layout.tsx
+++ apps/lend/src/app/dashboard/layout.tsx
@@ -1,13 +1,13 @@
 import { bannerEnabled, lendName } from "@bera/config";
-import { LaunchBanner } from "@bera/shared-ui";
+import { BerpsBanner } from "@bera/shared-ui";
 import { FooterSM } from "@bera/shared-ui/src/footer";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <section>
-      {/* {bannerEnabled && (
-        <LaunchBanner appName={lendName} className="-mt-8 mb-8" />
-      )} */}
+      {bannerEnabled && (
+        <BerpsBanner className="-mt-8 mb-8" />
+      )}
       <div className="container min-h-minimun max-w-[1440px] pb-16">
         {children}
       </div>
