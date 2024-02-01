c53c1233a9bf98dfa0c86f611949ad4c0ceee4cb
mei <mei@berachain.com>
2024-02-01 14:41:58 -0500

    set up env

--- apps/ambassador/next.config.mjs
+++ apps/ambassador/next.config.mjs
@@ -1,4 +1,35 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import "./src/env.mjs";
 
-export default nextConfig;
+/** @type {import("next").NextConfig} */
+const config = {
+  reactStrictMode: true,
+  pageExtensions: ["ts", "tsx"],
+  transpilePackages: ["@bera/ui", "@bera/berajs"],
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
+  experimental: {
+    esmExternals: "loose",
+    webpackBuildWorker: true,
+  },
+  /** We already do linting and typechecking as separate tasks in CI */
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "s2.coinmarketcap.com",
+        port: "",
+        pathname: "/static/img/coins/**",
+      },
+    ],
+    domains: ["res.cloudinary.com", "raw.githubusercontent.com"],
+  },
+};
+
+export default config;
