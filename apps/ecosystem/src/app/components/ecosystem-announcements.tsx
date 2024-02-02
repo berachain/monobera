229af2676aac846dd17c7ab497ce855dc798d576
mei <mei@berachain.com>
2024-02-02 10:17:26 -0500

    add ecosystem boilerplate

---
+++ apps/ecosystem/src/app/components/ecosystem-announcements.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import React, { type ReactNode } from "react";
+import Link from "next/link";
+import { Button } from "@bera/ui/button";
+import { Skeleton } from "@bera/ui/skeleton";
+
+export default function EcosystemAnnouncements() {
+  const [allDataSize, setAllDataSize] = React.useState(4);
+  const isAllDataLoadingMore = false;
+  const isAllDataReachingEnd = false;
+  return (
+    <div className="relative flex h-fit flex-col items-center justify-center px-0 pb-0 pt-[64px]">
+      <div className="text-center text-3xl font-bold leading-[48px] text-foreground sm:text-5xl">
+        Ecosystem Announcements
+      </div>
+      <div className="mt-4 text-center text-lg font-semibold leading-7 text-muted-foreground sm:text-xl">
+        Upcoming Events, Hackathons, Team GetToGethers, etc..
+      </div>
+      <div className="mt-8 grid h-fit grid-cols-1 gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
+        {[1, 2, 3, 4].map((index) => (
+          <div className="relative col-span-1 flex h-[340px] w-[420px] flex-col rounded-xl border border-solid bg-background">
+            <div className="flex flex-col items-center gap-1 px-6 pb-4 pt-6">
+              <Skeleton className="rounded-2 h-[149px] w-[372px]" />
+              <div className="text-xl font-semibold text-foreground">
+                Project name
+              </div>
+            </div>
+            <div className="item-center flex w-full justify-center gap-1 p-3 pt-6">
+              <Link href="" target="_blank">
+                <Button className="w-fit" variant="secondary">
+                  View Project
+                </Button>
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="mt-8">
+        <Button
+          onClick={() => setAllDataSize(allDataSize + 1)}
+          disabled={isAllDataLoadingMore || isAllDataReachingEnd}
+          variant="outline"
+        >
+          Load more
+        </Button>
+      </div>
+    </div>
+  );
+}
---
+++ apps/ecosystem/src/app/components/hero.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "@bera/ui/button";
+
+export default function Hero() {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="relative mx-auto flex h-[483px] w-[1280px]">
+        <Image
+          src="/eco_bear_bg.png"
+          alt="Eco Bear"
+          width={1280}
+          height={483}
+          className="absolute left-0 top-0 h-full w-full"
+        />
+        <div className="absolute p-12">
+          <h1 className="text-3xl font-extrabold leading-8 tracking-tight text-foreground sm:text-4xl">
+            Berachain Ecosystem
+          </h1>
+          <h2 className="text-md mt-2 whitespace-nowrap leading-8 tracking-tight text-muted-foreground">
+            A hub for all the bAPPs and brojects Building on Berachain. Want to
+            contribute to the bearchain ecosystem?
+          </h2>
+
+          <div className="mt-4 flex gap-4">
+            <Link href={"/dashboard"}>
+              <Button
+                className="rounded-18 mt-8 w-full text-lg font-semibold leading-7 sm:w-auto"
+                size={"lg"}
+              >
+                Build on Berachain
+              </Button>
+            </Link>
+            <Link href={"/dashboard"}>
+              <Button
+                className="rounded-18 mt-8 w-full text-lg font-semibold leading-7 sm:w-auto"
+                size={"lg"}
+                variant="secondary"
+              >
+                Read about $150M Ecosystem Fund
+              </Button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
