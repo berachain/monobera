d7e8c61c2b4e921a8dd22ff1338fbafb42edb18f
NINI BEAR <141682447+ninibear030@users.noreply.github.com>
2024-02-05 13:37:51 -0500

    [for Ocean to review] Bendi (#574)

--- apps/lend/src/app/dashboard/layout.tsx
+++ apps/lend/src/app/dashboard/layout.tsx
@@ -1,11 +1,13 @@
-import { bannerEnabled } from "@bera/config";
-import { BerpsBanner } from "@bera/shared-ui";
+import { CustomizedBanner } from "@bera/shared-ui";
 import { FooterSM } from "@bera/shared-ui/src/footer";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <section>
-      {bannerEnabled && <BerpsBanner className="-mt-8 mb-8" appName={""} />}
+      <CustomizedBanner
+        className="-mt-8 mb-8"
+        text="BEND balances are currently being migrated. Previously supplied assets will reappear thoon."
+      />
       <div className="container min-h-minimun max-w-[1440px] pb-16">
         {children}
       </div>
