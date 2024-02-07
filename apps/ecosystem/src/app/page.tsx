722647e1093578f3ce15fade2c108d985f729a55
MeiTheBera <156114970+meithebear@users.noreply.github.com>
2024-02-07 11:21:55 -0500

    [TEST ONLY] Ecosystem page (#572)
    * save progress
    
    * add ecosystem boilerplate
    
    * add theme provider for ecosystem
    
    * linter
    
    * add mobile nav

---
+++ apps/ecosystem/src/app/page.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+import EcosystemAnnouncements from "./components/ecosystem-announcements";
+import EcosystemProjects from "./components/ecosystem-projects";
+import Hero from "./components/hero";
+import TopProjects from "./components/top-projects";
+
+export default function Home() {
+  return (
+    <main>
+      <div className="relative mx-auto flex flex-col items-center justify-between bg-contain bg-no-repeat">
+        <Hero />
+        <TopProjects />
+        <EcosystemProjects />
+        <EcosystemAnnouncements />
+      </div>
+    </main>
+  );
+}
