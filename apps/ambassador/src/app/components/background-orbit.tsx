d8b114d49f66232d56246fc4bdb8508caee6834c
mei <mei@berachain.com>
2024-02-07 17:32:03 -0500

    update package json

--- apps/ambassador/src/app/components/background-orbit.tsx
+++ apps/ambassador/src/app/components/background-orbit.tsx
@@ -8,7 +8,6 @@ import { Orbit } from "./orbit";
 
 export default function BackGroundOrbit() {
   const { theme, systemTheme } = useTheme();
-  console.log("theme", theme, systemTheme);
   const t = theme === "system" ? systemTheme : theme;
   const bgGradient =
     t === "dark"
