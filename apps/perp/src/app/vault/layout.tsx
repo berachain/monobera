21b5fc235b648de9b5011c4a6b9547491d4f6f28
brown <brown@berachain.com>
2024-02-11 17:41:36 -0500

    feat(perp): update relevant perps pages to include maintenance banner

--- apps/perp/src/app/vault/layout.tsx
+++ apps/perp/src/app/vault/layout.tsx
@@ -1,14 +1,12 @@
 import { bannerEnabled, perpsName } from "@bera/config";
-import { UpTimeStatus, LaunchBanner, CustomizedBanner } from "@bera/shared-ui";
+import { UpTimeStatus, LaunchBanner } from "@bera/shared-ui";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <section className="relative pb-[72px] ">
-      {bannerEnabled && <LaunchBanner appName={perpsName} className="mt-0" />}
-      <CustomizedBanner
-        className="mb-8 mt-0"
-        text="Scheduled Maintenance: Berps will undergo system updates on Feb 13th, 7:00PM to 8:00PM UTC. Please wind down open positions."
-      />
+      {bannerEnabled && (
+        <LaunchBanner appName={perpsName} className="mb-8 mt-0" />
+      )}
       <div className="container min-h-minimun max-w-1280">{children}</div>
       <UpTimeStatus />
     </section>
