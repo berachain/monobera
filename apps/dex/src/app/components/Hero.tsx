c21955424d09023517c43993cec5dc10c647fa87
NINI BEAR <141682447+ninibear030@users.noreply.github.com>
2024-02-12 10:12:05 -0500

    landing ui (#607)

--- apps/dex/src/app/components/Hero.tsx
+++ apps/dex/src/app/components/Hero.tsx
@@ -1,61 +1,81 @@
 "use client";
 
 import React from "react";
+import Image from "next/image";
 import Link from "next/link";
-import { docsUrl } from "@bera/config";
+import { cloudinaryUrl, docsUrl } from "@bera/config";
 import { Button } from "@bera/ui/button";
 import { Icons } from "@bera/ui/icons";
 
 export default function Hero() {
   return (
-    <div className="my-24 flex w-full flex-col items-center justify-between gap-4">
-      <div className="mb-2 text-center text-3xl font-extrabold md:text-left md:text-5xl">
-        <div className="text-center">
+    <div className="flex flex-col justify-between lg:flex-row">
+      <div className="lg:items-start items-center my-24 flex w-full lg:w-[450px] flex-col gap-8 ">
+        <div className="text-center text-6xl font-extrabold lg:text-left lg:text-5xl">
+          Swap Tokens <br />
+          on{" "}
           <span className="bg-gradient-to-r from-[#FFC738] to-[#FF8A00F5] bg-clip-text text-transparent">
-            Swap Tokens
+            {" "}
+            Berachain&apos;s
           </span>{" "}
-          on
+          <br />
+          Native
+          <span className="bg-gradient-to-r from-[#FFC738] to-[#FF8A00F5] bg-clip-text text-transparent">
+            {" "}
+            AMM
+          </span>
         </div>
-        <div>
-          Your Favorite <br className="block md:hidden" /> New Dex
-        </div>
-      </div>
 
-      <div className="mb-6 text-center md:text-left">
-        <Link href={"/swap"} className="shadow-dark-shadow">
-          <Button className="mr-3" variant={"primary"}>
-            Swap Tokens
-          </Button>
-        </Link>
-        <Link href={"/pools"} className="shadow-dark-shadow">
-          <Button variant="outline">View Pools</Button>
-        </Link>
-      </div>
+        <div className="text-2xl font-semibold leading-8 text-muted-foreground">
+          Swap tokens, add liquidity, create <br />
+          pools, and earn BGT rewards.
+        </div>
 
-      <div className="flex flex-col items-center gap-3 lg:flex-row">
-        <Link
-          className="flex h-8 w-fit items-center gap-1 rounded-2xl border border-border bg-muted px-2 text-base font-normal leading-normal text-secondary-foreground hover:cursor-pointer hover:text-foreground"
-          href={"/pools"}
-        >
-          💧 Add Liquidity to Earn BGT Rewards{" "}
-          <Icons.arrowRight className="h-3 w-3" />
-        </Link>
-        <Link
-          className="flex h-8 w-fit items-center gap-1 rounded-2xl border border-border bg-muted px-2 text-base font-normal leading-normal text-secondary-foreground hover:cursor-pointer hover:text-foreground"
-          href={"/pools/create"}
-        >
-          🏖️ Create your own Liquidity Pools{" "}
-          <Icons.arrowRight className="h-3 w-3" />
-        </Link>
-        <div
-          className="flex h-8 w-fit items-center gap-1 rounded-2xl border border-border bg-muted px-2 text-base font-normal leading-normal text-secondary-foreground hover:cursor-pointer hover:text-foreground"
-          onClick={() =>
-            window.open(`${docsUrl}/learn/protocol/bgt-emissions`, "_blank")
-          }
-        >
-          🐝 Learn about BGT emissions <Icons.arrowRight className="h-3 w-3" />
+        <div className="text-center lg:text-left flex gap-3">
+          <Link href={"/swap"} className="shadow-dark-shadow">
+            <Button variant={"primary"}>Swap Tokens</Button>
+          </Link>
+          <Link href={"/pools"} className="shadow-dark-shadow">
+            <Button variant="outline">
+              {" "}
+              <Icons.search className="w-6 h-6 mr-1 inline-block" /> View Pools
+            </Button>
+          </Link>
+        </div>
+
+        <div className="flex flex-col items-center gap-3 lg:items-start lg:hidden">
+          <Link
+            className="flex h-8 w-fit items-center gap-1 rounded-2xl border border-border bg-muted px-2 text-base font-normal leading-normal text-secondary-foreground hover:cursor-pointer hover:text-foreground"
+            href={"/pools"}
+          >
+            💧 Add Liquidity to Earn BGT Rewards{" "}
+            <Icons.arrowRight className="h-3 w-3" />
+          </Link>
+          <Link
+            className="flex h-8 w-fit items-center gap-1 rounded-2xl border border-border bg-muted px-2 text-base font-normal leading-normal text-secondary-foreground hover:cursor-pointer hover:text-foreground"
+            href={"/pools/create"}
+          >
+            🏖️ Create your own Liquidity Pools{" "}
+            <Icons.arrowRight className="h-3 w-3" />
+          </Link>
+          <div
+            className="flex h-8 w-fit items-center gap-1 rounded-2xl border border-border bg-muted px-2 text-base font-normal leading-normal text-secondary-foreground hover:cursor-pointer hover:text-foreground"
+            onClick={() =>
+              window.open(`${docsUrl}/learn/protocol/bgt-emissions`, "_blank")
+            }
+          >
+            🐝 Learn about BGT emissions{" "}
+            <Icons.arrowRight className="h-3 w-3" />
+          </div>
         </div>
       </div>
+      <Image
+        // className=""
+        src={`${cloudinaryUrl}/DEX/g0ntgyc2atxltdycxymz`}
+        alt="Create a pool screenshot"
+        width={610}
+        height={700}
+      />
     </div>
   );
 }
