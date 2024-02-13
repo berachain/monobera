6928543f69f8d7f0bff21cbcf42bbf6fe941f008
mei <mei@berachain.com>
2024-02-13 12:16:57 -0500

    styling ecosystem

--- apps/ecosystem/src/app/components/hero.tsx
+++ apps/ecosystem/src/app/components/hero.tsx
@@ -8,25 +8,29 @@ import { useTheme } from "next-themes";
 
 export default function Hero() {
   const { theme, systemTheme } = useTheme();
-  const t = theme === "system" ? systemTheme : theme;
+  const t = !theme || theme === "system" ? systemTheme || "dark" : theme;
+  const bg = t === "dark" ? "/dark_bear_bg.png" : "/light_bear_bg.png";
   return (
     <div className="flex items-center justify-center">
       <div className="relative mx-auto flex h-[483px] w-[1280px]">
         <Image
-          src={t === "dark" ? "/dark_bear_bg.png" : "/light_bear_bg.png"}
+          src={bg}
           alt="Eco Bear"
           width={1280}
           height={483}
           className="absolute left-0 top-0 h-full w-full"
         />
         <div className="absolute flex flex-col flex-wrap items-start py-28">
-          <div className="flex flex-col">
-            <h1 className="text-4xl font-extrabold leading-8 tracking-tight text-foreground sm:text-4xl">
-              Berachain Ecosystem
+          <div className="left-0 top-0 flex flex-col leading-10">
+            <h1 className="text-4xl font-extrabold leading-[72px] tracking-tight text-foreground sm:text-4xl">
+              Berachain
+              <br />
+              Ecosystem
             </h1>
             <h2 className="text-md mt-2 whitespace-nowrap leading-8 tracking-tight text-muted-foreground">
-              A hub for all the bAPPs and brojects Building on Berachain. Want
-              to contribute to the bearchain ecosystem?
+              A hub for all the bAPPs and brojects Building on Berachain.
+              <br />
+              Want to contribute to the bearchain ecosystem?
             </h2>
           </div>
 
--- apps/ecosystem/src/app/components/top-projects.tsx
+++ apps/ecosystem/src/app/components/top-projects.tsx
@@ -24,16 +24,22 @@ const topSections = [
 
 export default function TopProjects() {
   return (
-    <div className="relative h-fit px-8 pb-0">
-      <div className="mt-4 grid h-fit grid-cols-1 gap-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+    <div className="relative h-fit">
+      {/* <div className="mt-4 grid h-fit grid-cols-1 flex-col items-center justify-end gap-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"> */}
+      <div className="mt-4 flex items-center justify-center gap-12 lg:flex-row">
         {topSections.map((section) => (
-          <div className="item-left flex w-full gap-2 p-3 pt-6">
-            <Link href={section.link} target="_blank">
-              <Button className="h-[120px] w-[276px]" variant="secondary">
-                {section.title} <Icons.arrowRight />
-              </Button>
-            </Link>
-          </div>
+          <Link href={section.link} target="_blank">
+            <div className="relative flex h-[120px] w-[280px] self-stretch rounded-lg border border-solid bg-muted pb-5 pt-0">
+              <div className="flex items-end justify-center gap-20 self-stretch px-6">
+                <div className="font-lg relative w-[80px] font-bold">
+                  {section.title}
+                </div>
+                <div className="relative h-6 w-6 pb-4">
+                  <Icons.arrowRight />
+                </div>
+              </div>
+            </div>
+          </Link>
         ))}
       </div>
     </div>
