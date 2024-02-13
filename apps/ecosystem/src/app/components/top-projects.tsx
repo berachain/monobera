b79395846bf48f2466cc855878c18bd3924242b2
mei <mei@berachain.com>
2024-02-13 13:29:46 -0500

    styling + nav update

--- apps/ecosystem/src/app/components/top-projects.tsx
+++ apps/ecosystem/src/app/components/top-projects.tsx
@@ -24,9 +24,8 @@ const topSections = [
 
 export default function TopProjects() {
   return (
-    <div className="relative h-fit">
-      {/* <div className="mt-4 grid h-fit grid-cols-1 flex-col items-center justify-end gap-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"> */}
-      <div className="mt-4 flex items-center justify-center gap-12 lg:flex-row">
+    <div className="relative h-fit flex-wrap ">
+      <div className="mt-4 flex flex-col flex-wrap items-center justify-center gap-12 lg:flex-row">
         {topSections.map((section) => (
           <Link href={section.link} target="_blank">
             <div className="relative flex h-[120px] w-[280px] self-stretch rounded-lg border border-solid bg-muted pb-5 pt-0">
