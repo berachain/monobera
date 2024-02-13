7020d4a7b3f3b3c06d932b397a3567dd8bb62f3e
mei <mei@berachain.com>
2024-02-13 12:40:30 -0500

    remove useValidatorInfo

--- packages/shared-ui/src/validator-icon.tsx
+++ packages/shared-ui/src/validator-icon.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import Image from "next/image";
-import { useValidatorIcon, useValidators } from "@bera/berajs";
+import { useValidatorIcon } from "@bera/berajs";
 import { cn } from "@bera/ui";
 import { Avatar, AvatarFallback, AvatarImage } from "@bera/ui/avatar";
 import { type Address } from "viem";
@@ -16,34 +16,19 @@ export const ValidatorIcon = ({
   description?: string;
   className?: string;
 }) => {
-  const validatorInfo = useValidators();
-  const localValidatorImg = validatorInfo?.validatorDictionary
-    ? validatorInfo?.validatorDictionary[address]?.logoURI
-    : "";
-
   const { data: validatorImg } = useValidatorIcon(description, address);
 
   return (
     <Avatar className={cn("", className)}>
       <AvatarImage src={`${process.env.NEXT_PUBLIC_AWS_URL}/${validatorImg}`} />
       <AvatarFallback>
-        {localValidatorImg ? (
-          <Image
-            src={localValidatorImg}
-            width={100}
-            height={100}
-            className="h-full w-full rounded-full border border-border p-1"
-            alt={"validator-icon"}
-          />
-        ) : (
-          <Image
-            src={`${process.env.NEXT_PUBLIC_CLOUDINARY_URL}/shared/wx4snihxcxxdko2wpsbj`}
-            width={100}
-            height={100}
-            className="h-full w-full rounded-full border border-border p-1"
-            alt={"validator-icon"}
-          />
-        )}
+        <Image
+          src={`${process.env.NEXT_PUBLIC_CLOUDINARY_URL}/shared/wx4snihxcxxdko2wpsbj`}
+          width={100}
+          height={100}
+          className="h-full w-full rounded-full border border-border p-1"
+          alt={"validator-icon"}
+        />
       </AvatarFallback>
     </Avatar>
   );
