8873e235e1c7f535b38e0bf4e0e0f1b373bf8aed
nina yang <bina@berachain.com>
2024-02-13 15:24:08 -0500

    no ssr wrapper

---
+++ apps/faucet/src/components/no-ssr-wrapper.tsx
@@ -0,0 +1,8 @@
+import dynamic from "next/dynamic";
+
+const NonSSRWrapper = (props: any) => (
+  <>{props.children}</>
+);
+export default dynamic(() => Promise.resolve(NonSSRWrapper), {
+  ssr: false,
+});
