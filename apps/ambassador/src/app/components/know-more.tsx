096185d043b0112b389c54116f0df35e81c2be51
mei <mei@berachain.com>
2024-02-16 14:52:30 -0500

    responsiveness

--- apps/ambassador/src/app/components/know-more.tsx
+++ apps/ambassador/src/app/components/know-more.tsx
@@ -13,14 +13,15 @@ export default function KnowMore() {
           <div className="absolute flex flex-col flex-wrap items-center justify-center pr-8 sm:items-start">
             <div className="top-0 flex flex-col flex-wrap leading-10 lg:left-0">
               <h1 className="text-lg font-extrabold tracking-tight text-foreground sm:text-2xl sm:leading-[72px]">
-                Get My Project Listed
+                Want to know more?
               </h1>
-              <h2 className="flex-wrap text-sm tracking-tight text-muted-foreground sm:text-lg sm:leading-8">
-                Fellow builder in the making? Submit project details via this
-                form <br />
-                and please be sure to provide us as much information as
-                possible.
-              </h2>
+              <div className="max-w-[480px] flex-wrap lg:w-full">
+                <h2 className="flex-wrap text-sm tracking-tight text-muted-foreground sm:text-lg sm:leading-8">
+                  Fellow builder in the making? Submit project details via this
+                  form and please be sure to provide us as much information as
+                  possible.
+                </h2>
+              </div>
             </div>
 
             <div className="flex items-center lg:mt-8">
@@ -28,23 +29,25 @@ export default function KnowMore() {
                 <Button
                   className="rounded-18 mt-4 w-full text-lg font-semibold leading-7 sm:w-auto"
                   size={"sm"}
-                  variant="info"
+                  variant="outline"
                 >
-                  Submit
+                  Learn More
                 </Button>
               </Link>
             </div>
           </div>
         </div>
-        <div className="hidden justify-center sm:flex sm:justify-end">
-          <Image
-            src="/knowmoarbear.png"
-            alt="Project"
-            width={400}
-            height={400}
-            layout="intrinsic"
-            className="xl:absolute xl:bottom-0 xl:right-[15%]"
-          />
+        <div className="relative hidden justify-end sm:flex">
+          <div className="absolute bottom-0 right-0 xl:right-[15%]">
+            <Image
+              src="/knowmoarbear.png"
+              alt="Project"
+              width={400}
+              height={400}
+              layout="intrinsic"
+              className="h-[200px] w-[200px] md:h-[300px] md:w-[300px] lg:h-auto lg:w-auto lg:max-w-full"
+            />
+          </div>
         </div>
       </div>
     </div>
