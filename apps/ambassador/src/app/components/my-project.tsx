3b4534b6c721d1e35a496c80d0d3afa7626e26f7
mei <mei@berachain.com>
2024-02-16 12:24:11 -0500

    responsiving

--- apps/ambassador/src/app/components/my-project.tsx
+++ apps/ambassador/src/app/components/my-project.tsx
@@ -8,15 +8,15 @@ import { Button } from "@bera/ui/button";
 export default function MyProject() {
   return (
     <div className="px-16 pb-16 pt-[64px]">
-      <div className="relative flex items-center justify-center gap-4 rounded-xl border border-solid bg-muted px-8 py-4">
+      <div className="relative">
         {" "}
-        <div className="relative mx-auto flex h-[500px] w-[480px] flex-wrap md:h-[220px] lg:w-[1080px]">
-          <div className="absolute flex flex-col flex-wrap items-start py-2">
+        <div className="relative mx-auto flex min-h-[300px] w-full flex-wrap overflow-hidden rounded-xl border border-solid bg-muted px-4 py-2 sm:px-8 md:h-[220px] md:max-w-[1080px]">
+          <div className="absolute flex flex-col flex-wrap items-center pr-8 sm:items-start">
             <div className="top-0 flex flex-col flex-wrap leading-10 lg:left-0">
-              <h1 className="text-2xl font-extrabold leading-[72px] tracking-tight text-foreground sm:text-2xl">
+              <h1 className="text-lg font-extrabold tracking-tight text-foreground sm:text-2xl sm:leading-[72px]">
                 Get My Project Listed
               </h1>
-              <h2 className="text-md mt-2 flex-wrap whitespace-nowrap leading-8 tracking-tight text-muted-foreground">
+              <h2 className="flex-wrap text-sm tracking-tight text-muted-foreground sm:text-lg sm:leading-8">
                 Fellow builder in the making? Submit project details via this
                 form <br />
                 and please be sure to provide us as much information as
@@ -24,11 +24,11 @@ export default function MyProject() {
               </h2>
             </div>
 
-            <div className="flex flex-wrap gap-4">
+            <div className="flex items-center lg:mt-8">
               <Link href={"/dashboard"}>
                 <Button
-                  className="rounded-18 mt-8 w-full text-lg font-semibold leading-7 sm:w-auto"
-                  size={"lg"}
+                  className="rounded-18 mt-4 w-full text-lg font-semibold leading-7 sm:w-auto"
+                  size={"sm"}
                   variant="info"
                 >
                   Submit
@@ -37,14 +37,14 @@ export default function MyProject() {
             </div>
           </div>
         </div>
-        <div className="flex justify-center sm:justify-end">
+        <div className="hidden justify-center sm:flex sm:justify-end">
           <Image
             src="/ecobear.png"
             alt="Project"
             width={650}
             height={600}
             layout="intrinsic"
-            className="lg:absolute lg:bottom-[-20%] lg:right-[-10%]"
+            className="xl:absolute xl:bottom-[-5%] xl:right-[10%]"
           />
         </div>
       </div>
