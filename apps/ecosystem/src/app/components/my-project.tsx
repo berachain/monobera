909657559b79ea1f551843c665662c6c3df31448
mei <mei@berachain.com>
2024-02-16 14:52:30 -0500

    responsiveness

--- apps/ecosystem/src/app/components/my-project.tsx
+++ apps/ecosystem/src/app/components/my-project.tsx
@@ -9,24 +9,25 @@ export default function MyProject() {
   return (
     <div className="px-0 pb-16 pt-[64px]">
       <div className="relative">
-        <div className="relative mx-auto flex min-h-[300px] min-w-[300px] gap-4 rounded-xl border border-solid bg-muted px-8 py-4 md:h-[220px] lg:w-[1080px]">
+        <div className="relative mx-auto flex min-h-[300px] min-w-[400px] gap-4 rounded-xl border border-solid bg-muted px-8 py-4 md:h-[220px] md:min-w-[800px] lg:w-[1080px]">
           <div className="absolute flex flex-col flex-wrap items-center justify-center pr-8 sm:items-start">
             <div className="top-0 flex flex-col flex-wrap leading-10 lg:left-0">
               <h1 className="text-lg font-extrabold tracking-tight text-foreground sm:text-2xl sm:leading-[72px]">
                 Get My Project Listed
               </h1>
-              <h2 className="flex-wrap text-sm tracking-tight text-muted-foreground sm:text-lg sm:leading-8">
-                Fellow builder in the making? Submit project details via this
-                form <br />
-                and please be sure to provide us as much information as
-                possible.
-              </h2>
+              <div className="max-w-[480px] flex-wrap lg:w-full">
+                <h2 className="flex-wrap text-sm tracking-tight text-muted-foreground sm:text-lg sm:leading-8">
+                  Fellow builder in the making? Submit project details via this
+                  form and please be sure to provide us as much information as
+                  possible.
+                </h2>
+              </div>
             </div>
 
-            <div className="flex flex-wrap gap-4 lg:mt-8">
+            <div className="flex flex-wrap lg:mt-8">
               <Link href={"/dashboard"}>
                 <Button
-                  className="rounded-18 mt-8 w-full text-lg font-semibold leading-7 sm:w-auto"
+                  className="rounded-18 mt-4 w-full text-lg font-semibold leading-7 sm:w-auto"
                   size={"sm"}
                   variant="info"
                 >
@@ -36,15 +37,17 @@ export default function MyProject() {
             </div>
           </div>
         </div>
-        <div className="hidden justify-center sm:flex sm:justify-end">
-          <Image
-            src="/ecobear.png"
-            alt="Project"
-            width={650}
-            height={600}
-            layout="intrinsic"
-            className="xl:absolute xl:bottom-[-5%] xl:right-[-10%]"
-          />
+        <div className="relative hidden justify-end sm:flex">
+          <div className="absolute bottom-0 right-0 xl:bottom-[-5%] xl:right-[-10%]">
+            <Image
+              src="/ecobear.png"
+              alt="Project"
+              width={600}
+              height={650}
+              layout="intrinsic"
+              className="h-[200px] w-[200px] md:h-[400px] md:w-[400px] lg:h-auto lg:w-auto lg:max-w-full"
+            />
+          </div>
         </div>
       </div>
     </div>
