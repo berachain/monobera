9dc58e5a8b6fd0f2563d89456b2d306f7c24e603
mei <mei@berachain.com>
2024-02-16 12:55:30 -0500

    ecosystem

--- apps/ambassador/src/app/components/my-project.tsx
+++ apps/ambassador/src/app/components/my-project.tsx
@@ -9,9 +9,8 @@ export default function MyProject() {
   return (
     <div className="px-16 pb-16 pt-[64px]">
       <div className="relative">
-        {" "}
         <div className="relative mx-auto flex min-h-[300px] w-full flex-wrap overflow-hidden rounded-xl border border-solid bg-muted px-8 py-4 md:h-[220px] md:max-w-[1080px]">
-          <div className="absolute flex flex-col flex-wrap items-center pr-8 sm:items-start">
+          <div className="absolute flex flex-col flex-wrap items-center justify-center pr-8 sm:items-start">
             <div className="top-0 flex flex-col flex-wrap leading-10 lg:left-0">
               <h1 className="text-lg font-extrabold tracking-tight text-foreground sm:text-2xl sm:leading-[72px]">
                 Get My Project Listed
