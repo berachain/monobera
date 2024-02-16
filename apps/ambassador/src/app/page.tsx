4611a8b5350544f3d09095e11dd21caf3a45f47a
mei <mei@berachain.com>
2024-02-16 11:12:12 -0500

    save progress

--- apps/ambassador/src/app/page.tsx
+++ apps/ambassador/src/app/page.tsx
@@ -1,17 +1,12 @@
 import React from "react";
 import { type Metadata } from "next";
 import { Footer } from "@bera/shared-ui";
-import { Button } from "@bera/ui/button";
 
-import {
-  AmbassadorCard,
-  brigadeGeneral,
-  colonel,
-  kingsOath,
-} from "./components/ambassador-tiers/tiers";
+import AmbassadorCards from "./components/ambassador-tiers/ambassadorCards";
 import BackGroundOrbit from "./components/background-orbit";
 import FAQ from "./components/faq";
 import Hero from "./components/hero";
+import MyProject from "./components/my-project";
 import SignUp from "./components/signUp-section/signup";
 import { Projects } from "./components/upcoming-events/events";
 
@@ -31,31 +26,11 @@ export default function Page() {
               <BackGroundOrbit />
             </div>
             <Projects />
-            <div className="flex flex-col items-center justify-center pb-12 pt-12">
-              <h1 className="md:leading-14 leading-24 pb-12 text-3xl font-extrabold md:text-5xl">
-                Tiers of an{" "}
-                <span className="bg-gradient-to-r from-[rgba(255,181,113,0.9)] to-[rgba(255,122,0,0.9)] bg-clip-text text-transparent backdrop-blur-md">
-                  Ambassador
-                </span>
-              </h1>
-              <div className="flex flex-wrap justify-center gap-8 p-4 pb-4">
-                <div className="order-2 sm:order-1">
-                  <AmbassadorCard ambassador={brigadeGeneral} />
-                </div>
-                <div className="order-1">
-                  <AmbassadorCard ambassador={kingsOath} />
-                </div>
-                <div className="order-3">
-                  <AmbassadorCard ambassador={colonel} />
-                </div>
-              </div>
-              <Button variant="outline" className="mb-8 mt-12">
-                Learn more about Tiers
-              </Button>
-            </div>
+            <AmbassadorCards />
           </div>
           <SignUp />
           <FAQ />
+          <MyProject />
           <Footer />
         </div>
       </div>
