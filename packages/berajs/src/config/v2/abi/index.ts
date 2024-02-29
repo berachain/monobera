3d36a552f31cc5ae3538e2aeebe0e515bae3d45c
mei <mei@berachain.com>
2024-02-29 17:30:05 -0500

    add new hooks

---
+++ packages/berajs/src/config/v2/abi/index.ts
@@ -0,0 +1,5 @@
+export * from "./bgt";
+export * from "./berachef";
+export * from "./distributor";
+export * from "./blockRewardController";
+export * from "./berachainRewardsVault";
---
+++ packages/berajs/src/hooks/v2/berachef/useDelegation.ts
@@ -0,0 +1,24 @@
+import useSWRImmutable from "swr/immutable";
+import { usePublicClient } from "wagmi";
+
+import { BERACHEF_ABI } from "~/config/v2/abi";
+import { useBeraConfig } from "~/contexts";
+
+export const useDelegation = (validatorAddress: `0x${string}`) => {
+  const publicClient = usePublicClient();
+  const { networkConfig } = useBeraConfig();
+
+  const method = "getDelegation";
+  // validatorCoinbaseAddr
+  const QUERY_KEY = [validatorAddress, method];
+  return useSWRImmutable(QUERY_KEY, async () => {
+    const result = await publicClient.readContract({
+      address: "0x0", // Berachef contract address
+      abi: BERACHEF_ABI,
+      functionName: method,
+      args: [validatorAddress],
+    });
+
+    return result;
+  });
+};
