78b63980968f8b625f2f54059d88ede87d30b550
mei <mei@berachain.com>
2024-02-29 17:30:05 -0500

    add new hooks

---
+++ packages/berajs/src/hooks/v2/bgtRewardsVault/usePollBGTReward.ts
@@ -0,0 +1,50 @@
+import useSWR, { mutate } from "swr";
+import useSWRImmutable from "swr/immutable";
+import { usePublicClient } from "wagmi";
+
+import POLLING from "~/config/constants/polling";
+import { BERACHAIN_REWARDS_VAULT_ABI } from "~/config/v2/abi";
+import { useBeraConfig } from "~/contexts";
+
+export const usePollBGTReward = (userAddress: `0x${string}`) => {
+  const method = "rewards";
+  const QUERY_KEY = [method, userAddress];
+  const publicClient = usePublicClient();
+  const { isLoading } = useSWR(
+    QUERY_KEY,
+    async () => {
+      try {
+        const bgtReward = await publicClient.readContract({
+          address: "0x0", // TODO: BERACHAIN_REWARDS_VAULT contract address
+          abi: BERACHAIN_REWARDS_VAULT_ABI,
+          functionName: method,
+          args: [],
+        });
+
+        if (!bgtReward) {
+          return undefined;
+        }
+        return {
+          bgtReward,
+        };
+      } catch (e) {
+        console.log(e);
+        return undefined;
+      }
+    },
+    {
+      refreshInterval: POLLING.NORMAL,
+    },
+  );
+
+  const useBGTReward = () => {
+    const { data = undefined } = useSWRImmutable<number | undefined>(QUERY_KEY);
+    return data;
+  };
+
+  return {
+    isLoading,
+    useBGTReward,
+    refresh: () => void mutate(QUERY_KEY),
+  };
+};
