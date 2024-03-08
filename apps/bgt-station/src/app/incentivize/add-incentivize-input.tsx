abd9d8b7485d81a8ec9278d4acaabac67f14cdc9
mei <mei@berachain.com>
2024-03-07 22:00:27 -0500

    save progress

---
+++ apps/bgt-station/src/app/incentivize/add-incentivize-input.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { type Token } from "@bera/berajs";
+import { bgtTokenAddress } from "@bera/config";
+import { SelectToken } from "@bera/shared-ui";
+import { Button } from "@bera/ui/button";
+import { Icons } from "@bera/ui/icons";
+import { Input } from "@bera/ui/input";
+
+import { IncentivizeToken } from "~/hooks/useCreateIncentivizeTokens";
+
+type Props = {
+  incentivizeToken: IncentivizeToken;
+  index: number;
+  selectedTokens: Token[];
+  selectable?: boolean;
+  onTokenSelection: (token: Token | undefined, index: number) => void;
+  onRemove: (index: number) => void;
+  onTokenAmountChange: (index: number, amount: number) => void;
+};
+
+export default function AddIncentivizeToken({
+  incentivizeToken,
+  index,
+  selectedTokens,
+  onTokenSelection,
+  onRemove,
+  onTokenAmountChange,
+  selectable = true,
+}: Props) {
+  const handleTokenSelection = (token: Token | undefined) => {
+    onTokenSelection(token, index);
+  };
+  return (
+    <li className={"flex flex-row items-center justify-between gap-1 px-4"}>
+      <SelectToken
+        token={incentivizeToken.token}
+        onTokenSelection={handleTokenSelection}
+        selectedTokens={selectedTokens}
+        selectable={selectable}
+        filter={[bgtTokenAddress]}
+      />
+      <Input
+        type="number"
+        step="any"
+        min="0"
+        placeholder="0.0"
+        className="w-full grow self-end border-0 bg-transparent p-0 text-right text-lg font-semibold outline-none ring-0 ring-offset-0 focus-visible:ring-0 focus-visible:ring-offset-0"
+        value={
+          Number(incentivizeToken.amount) > 0 ? incentivizeToken.amount : ""
+        }
+        onChange={(e) => {
+          onTokenAmountChange(index, Number(e.target.value));
+        }}
+      />
+      <Button
+        variant="ghost"
+        className="rounded-full p-0 hover:bg-transparent hover:text-red-500"
+        onClick={() => onRemove(index)}
+      >
+        <Icons.close className="h-4 w-4 " />
+      </Button>
+    </li>
+  );
+}
