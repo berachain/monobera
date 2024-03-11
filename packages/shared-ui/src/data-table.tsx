a690be07a1e7dbb4c97d69bbec00f2765bfd2c37
mei <mei@berachain.com>
2024-03-11 16:09:47 -0400

    merge conflict

--- packages/shared-ui/src/data-table.tsx
+++ packages/shared-ui/src/data-table.tsx
@@ -125,7 +125,7 @@ export function DataTable<TData, TValue>({
     >
       <div
         className={cn(
-          "w-full overflow-x-auto overflow-y-auto",
+          "w-full overflow-x-auto overflow-y-auto border-border",
           embedded ? "" : "rounded-tl-md rounded-tr-md",
         )}
       >
