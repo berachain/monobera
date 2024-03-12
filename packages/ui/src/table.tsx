3a5b0894e63df06259a75409a53cdfc5deadbe23
Pookie Bear <Khaledvsall@gmail.com>
2024-03-12 09:17:27 -0400

    feat: implement sorting and keyword search for All Pools tab in pools page (#720)
    * fix: search when bar is empty
    
    * remove sentryclirc
    
    * remove min height of table

--- packages/ui/src/table.tsx
+++ packages/ui/src/table.tsx
@@ -55,7 +55,7 @@ const TableRow = React.forwardRef<
   <tr
     ref={ref}
     className={cn(
-      "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
+      "transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted [&:not(:last-child)]:border-b",
       className,
     )}
     {...props}
