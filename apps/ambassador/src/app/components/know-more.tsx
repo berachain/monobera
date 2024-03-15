87653d586ffbae86ec150e0cc17b083f69aec02b
mei <mei@berachain.com>
2024-03-15 16:45:00 -0400

    center text + add padding

--- apps/ambassador/src/app/components/know-more.tsx
+++ apps/ambassador/src/app/components/know-more.tsx
@@ -9,13 +9,13 @@ export default function KnowMore() {
   return (
     <div className="p-8 lg:mt-8">
       <div className="mx-auto flex w-full flex-col justify-between rounded-xl border border-solid bg-muted sm:min-h-[300px] sm:flex-row sm:px-4 md:h-[220px] md:max-w-[1080px]">
-        <div className="flex flex-col items-center justify-center px-4 sm:items-start">
+        <div className="flex flex-col items-center justify-center p-4 sm:items-start">
           <div className="top-0 flex flex-col items-center leading-7 sm:items-start lg:left-0">
             <h1 className="text-lg font-extrabold tracking-tight text-foreground sm:text-2xl sm:leading-[72px]">
               Want to know more?
             </h1>
             <div className="flex-wrap items-center sm:max-w-[300px] md:max-w-[440px] lg:w-full">
-              <h2 className="text-md flex-wrap tracking-tight text-muted-foreground sm:text-lg">
+              <h2 className="text-md flex-wrap text-center tracking-tight text-muted-foreground sm:text-left sm:text-lg">
                 Interested in becoming a Berachain Ambassador? Learn more about
                 joining our innovative community and leading the blockchain
                 revolution.
