a0ad3991f9ee56faecd0d718574b001adb9456dd
MeiTheBera <156114970+meithebear@users.noreply.github.com>
2024-03-21 14:51:05 -0400

    feat: Ambassador UI (#764)
    * fix stuff
    
    * remove
    
    * rename back
    
    * fix text
    
    * my css improved
    
    * improved a little bit
    
    * remove wrap in hero
    
    * css continue to improve
    
    * center text + add padding
    
    * fix text position
    
    * fix ui
    
    * LearnMorePage-Responsiveness
    
    * format
    
    * address comments
    
    * update stuff
    
    * fix text color
    
    * add links
    
    ---------
    
    Co-authored-by: Coin Bera <coin@berachain.com>

--- apps/ambassador/src/app/components/know-more.tsx
+++ apps/ambassador/src/app/components/know-more.tsx
@@ -7,47 +7,43 @@ import { Button } from "@bera/ui/button";
 
 export default function KnowMore() {
   return (
-    <div className="px-16 pb-16 pt-[64px]">
-      <div className="relative">
-        <div className="relative mx-auto flex min-h-[300px] w-full flex-wrap overflow-hidden rounded-xl border border-solid bg-muted px-8 py-4 md:h-[220px] md:max-w-[1080px]">
-          <div className="absolute flex flex-col flex-wrap items-center justify-center pl-4 sm:items-start">
-            <div className="top-0 flex flex-col flex-wrap items-center leading-10 sm:items-start lg:left-0">
-              <h1 className="text-lg font-extrabold tracking-tight text-foreground sm:text-2xl sm:leading-[72px]">
-                Want to know more?
-              </h1>
-              <div className="max-w-[300px] flex-wrap items-center md:max-w-[440px] lg:w-full">
-                <h2 className="flex-wrap text-sm tracking-tight text-muted-foreground sm:text-lg sm:leading-8">
-                  Fellow builder in the making? Submit project details via this
-                  form and please be sure to provide us as much information as
-                  possible.
-                </h2>
-              </div>
+    <div className="p-8 lg:mt-8">
+      <div className="mx-auto flex w-full flex-col justify-between rounded-xl border border-solid bg-muted sm:min-h-[300px] sm:flex-row sm:px-4 md:h-[220px] md:max-w-[1080px]">
+        <div className="flex flex-col items-center justify-center p-4 sm:items-start">
+          <div className="top-0 flex flex-col items-center leading-7 sm:items-start lg:left-0">
+            <h1 className="text-lg font-extrabold tracking-tight text-foreground sm:text-2xl sm:leading-[72px]">
+              Want to know more?
+            </h1>
+            <div className="flex-wrap items-center sm:max-w-[300px] md:max-w-[440px] lg:w-full">
+              <h2 className="text-md flex-wrap text-center tracking-tight text-muted-foreground sm:text-left sm:text-lg">
+                Interested in becoming a Berachain Ambassador? Learn more about
+                joining our innovative community and leading the blockchain
+                revolution.
+              </h2>
             </div>
+          </div>
 
-            <div className="flex items-center lg:mt-8">
-              <Link href="/program-details">
-                <Button
-                  className="rounded-18 mt-4 w-full text-lg font-semibold leading-7 sm:w-auto"
-                  size={"sm"}
-                  variant="outline"
-                >
-                  Learn More
-                </Button>
-              </Link>
-            </div>
+          <div className="m-2 flex items-start lg:mt-8">
+            <Link href="/program-details">
+              <Button
+                className="rounded-18 mt-4 w-full text-lg font-semibold leading-7 sm:w-auto"
+                size={"sm"}
+                variant="outline"
+              >
+                Learn More
+              </Button>
+            </Link>
           </div>
         </div>
-        <div className="relative hidden justify-end sm:flex">
-          <div className="absolute bottom-0 right-0 xl:right-[15%]">
-            <Image
-              src="/knowmoarbear.png"
-              alt="Project"
-              width={400}
-              height={400}
-              layout="intrinsic"
-              className="h-[200px] w-[200px] md:h-[300px] md:w-[300px] lg:h-auto lg:w-auto lg:max-w-full"
-            />
-          </div>
+        <div className="hidden items-end sm:flex">
+          <Image
+            src="/knowmoarbear.png"
+            alt="Project"
+            width={400}
+            height={400}
+            layout="intrinsic"
+            className="h-[200px] w-[200px] md:h-[300px] md:w-[300px] lg:h-auto lg:w-auto"
+          />
         </div>
       </div>
     </div>
