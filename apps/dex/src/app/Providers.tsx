6bb47365a66c7c92d64b27d36514834cf3302a6e
ninibear030 <bina@berachain.com>
2024-03-25 17:09:40 -0400

    will this fix anyrhing

--- apps/dex/src/app/Providers.tsx
+++ apps/dex/src/app/Providers.tsx
@@ -2,18 +2,14 @@
 
 import React from "react";
 import { BeraConfig } from "@bera/berajs";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { ThemeProvider } from "~/components/theme-provider";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  const queryClient = new QueryClient();
   return (
     <BeraConfig>
       <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-        <QueryClientProvider client={queryClient}>
-          {children}
-        </QueryClientProvider>
+        {children}
       </ThemeProvider>
     </BeraConfig>
   );
