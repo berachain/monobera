d297edd61f76ee55e21f2dc9faaf8d3294377eb4
Khaled Ali <Khaledvsall@gmail.com>
2024-04-05 14:12:42 -0400

    make it backwards compatible

--- packages/ui/src/input.tsx
+++ packages/ui/src/input.tsx
@@ -8,12 +8,13 @@ export interface CustomInputProps extends InputProps {
   startAdornment?: React.ReactNode;
   endAdornment?: React.ReactNode;
   outerClassName?: string;
-  /** @param allowScientific - if @type is "number", this allows scientific notation by allowing the characters "e" or "E" to be inserted into the input */
+  /** @param allowScientific - if @type is "number-enhanced", this allows scientific notation by allowing the characters "e" or "E" to be inserted into the input */
   allowScientific?: boolean;
-  /** @param allowMinus - if @type is "number", this allows negative numbers by allowing the character "-" to be inserted into the input */
+  /** @param allowMinus - if @type is "number-enhanced", this allows negative numbers by allowing the character "-" to be inserted into the input */
   allowMinus?: boolean;
-  /** @param allowDecimal - if @type is "number", this allows negative numbers by allowing the character "." to be inserted into the input */
+  /** @param allowDecimal - if @type is "number-enhanced", this allows negative numbers by allowing the character "." to be inserted into the input */
   allowDecimal?: boolean;
+  type?: InputProps["type"] | "number-enhanced";
 }
 const Input = React.forwardRef<HTMLInputElement, CustomInputProps>(
   (
@@ -39,16 +40,16 @@ const Input = React.forwardRef<HTMLInputElement, CustomInputProps>(
       return formatted.join("");
     };
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      if (type === "number" && !allowScientific) {
+      if (type === "number-enhanced" && !allowScientific) {
         e.target.value = e.target.value?.replaceAll(/[eE]/g, "");
       }
-      if (type === "number" && !allowMinus) {
+      if (type === "number-enhanced" && !allowMinus) {
         e.target.value = e.target.value?.replace("-", "");
       }
-      if (type === "number" && !allowDecimal) {
+      if (type === "number-enhanced" && !allowDecimal) {
         e.target.value = e.target.value?.replace(".", "");
       }
-      if (type === "number") {
+      if (type === "number-enhanced") {
         e.target.value = e.target.value?.replaceAll(/[^eE\d.-]+/g, "");
         if (e.target.value?.split(".").length > 2) {
           // abort if more than one decimal in number
@@ -71,19 +72,23 @@ const Input = React.forwardRef<HTMLInputElement, CustomInputProps>(
     };
 
     const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-      if (type === "number" && !allowMinus && e.key === "-") {
+      if (type === "number-enhanced" && !allowMinus && e.key === "-") {
         e.preventDefault();
         return;
       }
-      if (type === "number" && !allowScientific && ["e", "E"].includes(e.key)) {
+      if (
+        type === "number-enhanced" &&
+        !allowScientific &&
+        ["e", "E"].includes(e.key)
+      ) {
         e.preventDefault();
         return;
       }
-      if (type === "number" && !allowDecimal && e.key === ".") {
+      if (type === "number-enhanced" && !allowDecimal && e.key === ".") {
         e.preventDefault();
         return;
       }
-      if (type === "number" && /[^eE\d.-]+/.test(e.key)) {
+      if (type === "number-enhanced" && /[^eE\d.-]+/.test(e.key)) {
         return;
       }
       onKeyDown?.(e);
@@ -97,7 +102,7 @@ const Input = React.forwardRef<HTMLInputElement, CustomInputProps>(
           </div>
         )}
         <input
-          type={type !== "number" ? type : undefined}
+          type={type === "number-enhanced" ? "number" : type}
           className={cn(
             "focus:border-1 flex h-10 w-full rounded-md border border-border px-3 py-2 text-sm text-foreground ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus:border-foreground focus:outline-none disabled:cursor-not-allowed disabled:text-muted-foreground",
             className,
