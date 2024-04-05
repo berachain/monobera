28c357b89f2bfdc04dd8ca555761865db7c7c991
Khaled Ali <Khaledvsall@gmail.com>
2024-04-05 15:03:22 -0400

    fix number type

--- packages/ui/src/input.tsx
+++ packages/ui/src/input.tsx
@@ -102,7 +102,7 @@ const Input = React.forwardRef<HTMLInputElement, CustomInputProps>(
           </div>
         )}
         <input
-          type={type === "number-enhanced" ? "number" : type}
+          type={type === "number-enhanced" ? undefined : type}
           className={cn(
             "focus:border-1 flex h-10 w-full rounded-md border border-border px-3 py-2 text-sm text-foreground ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus:border-foreground focus:outline-none disabled:cursor-not-allowed disabled:text-muted-foreground",
             className,
