bea8b11620eae68843093b7dc696d4868334cb39
Pookie Bear <Khaledvsall@gmail.com>
2024-04-11 13:19:29 -0400

    fix(dex): fix token dialog scroll and dialog height overflow (#862)
    * fix overflow in token selection dialog
    
    * fix for mobile height

--- packages/shared-ui/src/token-dialog.tsx
+++ packages/shared-ui/src/token-dialog.tsx
@@ -22,10 +22,10 @@ import { Icons } from "@bera/ui/icons";
 import { Balancer } from "react-wrap-balancer";
 import { isAddress } from "viem";
 
+import { FormattedNumber } from "./formatted-number";
 import { SearchInput } from "./search-input";
 import { TokenChip } from "./token-chip";
 import { TokenIcon } from "./token-icon";
-import { FormattedNumber } from "./formatted-number";
 
 type Props = {
   open: boolean;
@@ -162,7 +162,7 @@ export function TokenDialog({
     <>
       <Dialog open={open} onOpenChange={handleOpenChange}>
         {!managingTokens && (
-          <DialogContent className="flex max-w-[425px] flex-col gap-4 rounded-2xl px-6">
+          <DialogContent className="flex max-h-[100vh] max-w-[425px] flex-col gap-4 rounded-2xl px-6">
             <DialogHeader>
               <DialogTitle className="text-lg">Select a token</DialogTitle>
             </DialogHeader>
@@ -199,7 +199,7 @@ export function TokenDialog({
               )}
             </div>
             <div className="h-px w-full border-x-0 border-b-0 border-t border-solid border-border" />
-            <div className="overflow-y-scoll max-h-[600px] ">
+            <div className="max-h-[min(600px,60vh)] overflow-y-scroll">
               {!error ? (
                 filteredTokens?.length ? (
                   filteredTokens
@@ -247,7 +247,7 @@ export function TokenDialog({
           </DialogContent>
         )}
         {managingTokens && (
-          <DialogContent className="flex w-full flex-col">
+          <DialogContent className="flex max-h-[100vh] w-full  flex-col">
             <DialogHeader>
               <DialogTitle className="flex flex-row items-center justify-start gap-2 text-lg">
                 {" "}
@@ -342,7 +342,7 @@ const TokenDialogRow = ({
           <div className="absolute ml-auto" />
         )}
         {!pendingAddition && isConnected && (
-          <div className="ml-auto text-muted-foreground truncate">
+          <div className="ml-auto truncate text-muted-foreground">
             {/* <p>{tokenBalance ?? 0}</p> */}
             <FormattedNumber
               value={tokenBalance}
@@ -352,7 +352,7 @@ const TokenDialogRow = ({
           </div>
         )}
         <Dialog open={addTokenOpen} onOpenChange={setAddTokenOpen}>
-          <DialogContent className="flex flex-col items-center justify-center gap-3 px-4 md:w-[350px]">
+          <DialogContent className="flex max-h-[100vh] flex-col items-center justify-center gap-3 px-4  md:w-[350px]">
             <Icons.tooltip
               style={{ height: "64px", width: "64px", color: "#DC2626" }}
             />
