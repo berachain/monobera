9ce592ecac7a1139573874a216c5c17ffc54a17d
brown <brown@berachain.com>
2024-04-11 14:29:19 -0400

    feat(shared-ui): formatted number correct with negatives and other improvements

--- packages/shared-ui/src/tooltip.tsx
+++ packages/shared-ui/src/tooltip.tsx
@@ -14,30 +14,41 @@ export function Tooltip({
   className,
   size = 4,
   children,
+  toolTripTrigger,
   ...props
 }: {
   text?: any;
   className?: string;
   size?: number;
   children?: React.ReactNode;
+  toolTripTrigger?: React.ReactNode;
 }) {
   const [tooltipOpen, setTooltipOpen] = React.useState(false);
   return (
     <TooltipProvider>
       <BeraUiTooltip open={tooltipOpen} defaultOpen={false} {...props}>
         <TooltipTrigger asChild>
-          <Button
-            variant="ghost"
-            className={cn(
-              "flex-inline h-5 w-5 rounded-full p-0 align-middle hover:bg-transparent",
-              className,
-            )}
-            onMouseEnter={() => setTooltipOpen(true)}
-            onMouseLeave={() => setTooltipOpen(false)}
-          >
-            <Icons.tooltip className={cn(`h-${size} w-${size}`)} />
-            <span className="sr-only">Tooltip</span>
-          </Button>
+          {toolTripTrigger ? (
+            <div
+              onMouseEnter={() => setTooltipOpen(true)}
+              onMouseLeave={() => setTooltipOpen(false)}
+            >
+              {toolTripTrigger}
+            </div>
+          ) : (
+            <Button
+              variant="ghost"
+              className={cn(
+                "flex-inline h-5 w-5 rounded-full p-0 align-middle hover:bg-transparent",
+                className,
+              )}
+              onMouseEnter={() => setTooltipOpen(true)}
+              onMouseLeave={() => setTooltipOpen(false)}
+            >
+              <Icons.tooltip className={cn(`h-${size} w-${size}`)} />
+              <span className="sr-only">Tooltip</span>
+            </Button>
+          )}
         </TooltipTrigger>
         <TooltipContent className="w-fit max-w-[96vw]">
           {text}
