d123b6c00383a9de1bd42aa0474344936fef8d89
mei <mei@berachain.com>
2024-04-17 13:06:56 -0400

    add docs for actions

--- packages/berajs/src/actions/dex/getTransactionCount.ts
+++ packages/berajs/src/actions/dex/getTransactionCount.ts
@@ -1,16 +1,18 @@
 import { Address, PublicClient } from "viem";
 
+export interface IFetchTransactionCountArgs {
+  address: string | undefined;
+  publicClient: PublicClient | undefined;
+}
+
 /**
- * fetch the current bera balance of a given address
+ * fetch the transaction count of a given address
  */
 
 export const getTransactionCount = async ({
   address,
   publicClient,
-}: {
-  address: string | undefined;
-  publicClient: PublicClient | undefined;
-}): Promise<number | undefined> => {
+}: IFetchTransactionCountArgs): Promise<number | undefined> => {
   if (!publicClient) return undefined;
   if (address) {
     const transactionCount = await publicClient.getTransactionCount({
