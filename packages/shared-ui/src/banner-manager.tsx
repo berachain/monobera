b429c59b0a22a7f4ee2e9402a97ab944ef17e326
Mei Mei <156114970+meithebear@users.noreply.github.com>
2024-04-24 15:32:10 -0400

    use new banner sysem (#893)
    * add config to manager banner
    
    * format
    
    * refactor banner
    
    * fix dependency
    
    * create new banner config
    
    * format code
    
    * merge conflicts
    
    * use new banner sysem
    
    * add banner manager
    
    * switch back to false for all banner bool
    
    * rever unused change
    
    * refactor banner config
    
    * format
    
    * move banner config to shared-ui
    
    * sync with v2
    
    * fix(berajs): remove deprecated perp bot error message (#884)
    
    * refactor(berajs): implement new hook options (#889)
    
    * move berajsconfig default to berajs and integrate that change
    
    * mega super ultra massive hook refactor to make configs optional and use default from berajsprovider
    
    * remove unused imports
    
    * reinstate default polling where it used to be found
    
    * chore(berajs): update beraconfig related docs (#892)
    
    * small refactor getWalletBalances, docs moving stuff around (#894)
    
    * small refactor getWalletBalances, docs moving stuff around
    
    * fmt
    
    * refactor usePollWallet, useTokenHoneyPrices, useTokens
    
    * refactor txn count, honey and bera balance
    
    * Refactor more hooks
    
    * i forget
    
    * token search work again
    
    * some stuff
    
    * docs refactor
    
    * resolved all conflicts
    
    * update usePollTransactionCount
    
    * update docs
    
    * remove get honey balance && token address (#898)
    
    * remove get honey balance
    
    * lint
    
    * remove doc
    
    * sync with v2
    
    * add config to manager banner
    
    * create new banner config
    
    * merge conflicts
    
    * use new banner sysem
    
    * add banner manager
    
    * switch back to false for all banner bool
    
    * refactor banner config
    
    * sync with v2
    
    * sync with v2
    
    * sync
    
    * remove unused
    
    * address comments
    
    * remove styling
    
    * update readme for banner management
    
    ---------
    
    Signed-off-by: Mei Mei <156114970+meithebear@users.noreply.github.com>
    Co-authored-by: Brown Bear <157503602+BrownBrownBear@users.noreply.github.com>
    Co-authored-by: Pookie Bear <Khaledvsall@gmail.com>
    Co-authored-by: Garrett Parris <garrettp18@gmail.com>
    Co-authored-by: NINI BEAR <141682447+ninibear030@users.noreply.github.com>

---
+++ packages/shared-ui/src/banner-manager.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import React from "react";
+import { usePathname } from "next/navigation";
+
+import { DappBannerType, bannerConfig } from "./bannerConfig";
+
+interface BannerManagerProps {
+  appName: string | undefined;
+}
+
+const BannerManager = ({ appName }: BannerManagerProps) => {
+  const pathname = usePathname();
+
+  const shouldShowBanner = (type: DappBannerType, appName: string) => {
+    const globalBanner = bannerConfig.global?.[type];
+    const appBanner = bannerConfig[appName]?.[type];
+
+    const isGlobalEnabled =
+      globalBanner?.enabled &&
+      (!globalBanner.hrefs || globalBanner.hrefs.includes(pathname));
+    const isAppEnabled =
+      appBanner?.enabled &&
+      (!appBanner.hrefs || appBanner.hrefs.includes(pathname));
+
+    return isGlobalEnabled || isAppEnabled;
+  };
+
+  const appNameOrDefault = appName || "Berachain Dapps";
+
+  return (
+    <>
+      {Object.values(DappBannerType).map((type) => (
+        <div key={type}>
+          {shouldShowBanner(type, appNameOrDefault) &&
+            bannerConfig[appNameOrDefault]?.[type]?.bannerComponent}
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default BannerManager;
