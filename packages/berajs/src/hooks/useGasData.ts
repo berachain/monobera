78da33935d5b4e691ae8c6766597e5347fd3b2a3
Khaled Ali <Khaledvsall@gmail.com>
2024-05-07 15:22:11 -0400

    improve use gas data hook

--- packages/berajs/src/hooks/useGasData.ts
+++ packages/berajs/src/hooks/useGasData.ts
@@ -1,36 +1,92 @@
 import { useEffect, useState } from "react";
-import { EstimateContractGasParameters } from "viem";
+import { crocMultiSwapAddress, nativeTokenAddress } from "@bera/config";
+import {
+  ContractFunctionArgs,
+  EstimateContractGasParameters,
+  PublicClient,
+  formatEther,
+  parseEther,
+} from "viem";
 import { usePublicClient } from "wagmi";
 
+interface UseGasDataReturnType {
+  estimatedBeraFee: number | undefined;
+}
+
+const getGeneralGasEstimate = async (
+  publicClient: ReturnType<typeof usePublicClient>,
+) => {
+  return publicClient
+    ?.estimateGas({
+      account: nativeTokenAddress,
+      to: crocMultiSwapAddress,
+      value: parseEther("1"),
+    })
+    .then(async (gas: bigint) => {
+      const feesPerGasEstimate = await publicClient?.estimateFeesPerGas();
+      const estimatedTxFeeInBera =
+        feesPerGasEstimate.maxFeePerGas &&
+        parseFloat(`${feesPerGasEstimate.maxFeePerGas * gas}`) * 4;
+      return { estimatedTxFeeInBera: formatEther(estimatedTxFeeInBera) };
+    });
+};
+
+const getContractGasEstimate = async (
+  publicClient: ReturnType<typeof usePublicClient>,
+  contractArgs: any,
+) => {
+  return publicClient
+    ?.estimateContractGas({ ...(contractArgs as any) })
+    .then(async (gas) => {
+      const feesPerGasEstimate = await publicClient?.estimateFeesPerGas();
+      const estimatedTxFeeInBera =
+        feesPerGasEstimate.maxPriorityFeePerGas &&
+        parseFloat(`${feesPerGasEstimate.maxPriorityFeePerGas * gas}`) * 1.3;
+      return { estimatedTxFeeInBera: formatEther(estimatedTxFeeInBera) };
+    });
+};
+
+/**
+ * Hook that returns estimated gas data, for a general unspecified transaction or a specific one.
+ * When contract args are provided, performs a more exact estimate with a 30% safety margin, performs an 4x padded but inaccurate estimation if not.
+ * @param {ContractFunctionArgs} param0.contractArgs - contract args for a transaction that we want to estimate gas for.
+ * @returns {UseGasDataReturnType} - returns the estimated gas data
+ */
 export const useGasData = ({
   contractArgs,
 }: {
   contractArgs?: EstimateContractGasParameters<any> | null;
-} = {}): any => {
+} = {}): UseGasDataReturnType => {
   const publicClient = usePublicClient();
 
-  const [gasData, setGasData] = useState<bigint | undefined>();
+  const [estimatedBeraFee, setEstimatedBeraFee] = useState<
+    number | undefined
+  >();
   useEffect(() => {
     if (contractArgs === undefined) {
-      publicClient
-        ?.getGasPrice()
-        .then((generalGasPrice) => {
-          setGasData(BigInt(parseFloat(`${generalGasPrice}`)));
+      getGeneralGasEstimate(publicClient)
+        .then((res: { estimatedTxFeeInBera: number } | undefined) => {
+          if (!res) {
+            throw new Error("failed to get general gas estimate");
+          }
+          setEstimatedBeraFee(res.estimatedTxFeeInBera);
         })
         .catch();
       return;
     }
     if (!contractArgs) {
-      setGasData(undefined);
+      setEstimatedBeraFee(undefined);
       return;
     }
-    publicClient
-      ?.estimateContractGas({ ...(contractArgs as any) })
-      .then((data) => {
-        setGasData(data);
+    getContractGasEstimate(publicClient, contractArgs)
+      .then((res: { estimatedTxFeeInBera: number } | undefined) => {
+        if (!res) {
+          throw new Error("failed to get contract gas estimate");
+        }
+        setEstimatedBeraFee(res?.estimatedTxFeeInBera);
       })
       .catch((e: unknown) => {
-        setGasData(undefined);
+        setEstimatedBeraFee(undefined);
 
         const isFalseAlarm =
           `${e}`.includes("Unable to decode signature") ||
@@ -42,5 +98,5 @@ export const useGasData = ({
       });
   }, [contractArgs]);
 
-  return gasData;
+  return { estimatedBeraFee };
 };
