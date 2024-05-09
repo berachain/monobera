e1de6638a96f620f5d25a0b8df92dd8ed5f25b01
NINI BEAR <141682447+ninibear030@users.noreply.github.com>
2024-05-09 16:50:13 -0400

    feat: bgt station gauge ui (#953)
    * fix header
    
    * general vaults details
    
    * lint
    
    * gud stuff
    
    * mobile
    
    * dropdown
    
    * mobile
    
    * new section
    
    * remove console
    
    * i have no idea why it was working bedfore
    
    * add a space
    
    * mc
    
    * mc
    
    ---------
    
    Signed-off-by: NINI BEAR <141682447+ninibear030@users.noreply.github.com>

--- apps/bgt-station/src/app/gauge/gauge.tsx
+++ apps/bgt-station/src/app/gauge/gauge.tsx
@@ -1,55 +1,133 @@
 "use client";
 
-import React from "react";
-import Image from "next/image";
-import { cloudinaryUrl } from "@bera/config";
+import React, { useState } from "react";
+import { useBeraJs } from "@bera/berajs";
 import { SearchInput } from "@bera/shared-ui";
-import { Skeleton } from "@bera/ui/skeleton";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@bera/ui/tabs";
 
 import GlobalGaugeWeightChart from "~/components/global-gauge-weight-chart";
 import GlobalGaugeWeightTable from "~/components/global-gauge-weight-table";
 import GaugeInfoCard from "./gauge-info-card";
+import MarketSelector from "./market-selector";
+
+const mockChartData = [
+  {
+    label: "Gauge 1",
+    address: "0x101f52c804C1C02c0A1D33442ecA30ecb6fB2434",
+    percentage: 0.2,
+    amount: 100,
+  },
+  {
+    label: "Gauge 2",
+    address: "0x605fD73B339b82413B862966428eD8Ac52eC4227",
+    percentage: 0.3,
+    amount: 100,
+  },
+  {
+    label: "Gauge 3",
+    address: "0x751524E7bAdd31d018A4CAF4e4924a21b0c13CD0",
+    percentage: 0.5,
+    amount: 100,
+  },
+  {
+    label: "Gauge 4",
+    address: "0x751524E7bAdd31d018A4CAF4e4924a21b0c13CD0",
+    percentage: 0.5,
+    amount: 100,
+  },
+  {
+    label: "Gauge 5",
+    address: "0x751524E7bAdd31d018A4CAF4e4924a21b0c13CD0",
+    percentage: 0.5,
+    amount: 100,
+  },
+  {
+    label: "Gauge 6",
+    address: "0x751524E7bAdd31d018A4CAF4e4924a21b0c13CD0",
+    percentage: 0.5,
+    amount: 100,
+  },
+  {
+    label: "Gauge 7",
+    address: "0x751524E7bAdd31d018A4CAF4e4924a21b0c13CD0",
+    percentage: 0.5,
+    amount: 100,
+  },
+  {
+    label: "Gauge 8",
+    address: "0x751524E7bAdd31d018A4CAF4e4924a21b0c13CD0",
+    percentage: 0.039,
+    amount: 10,
+  },
+  {
+    label: "Gauge 9",
+    address: "0x751524E7bAdd31d018A4CAF4e4924a21b0c13CD0",
+    percentage: 0.039,
+    amount: 10,
+  },
+  {
+    label: "Gauge 10",
+    address: "0x751524E7bAdd31d018A4CAF4e4924a21b0c13CD0",
+    percentage: 0.039,
+    amount: 10,
+  },
+  {
+    label: "Gauge 11",
+    address: "0x751524E7bAdd31d018A4CAF4e4924a21b0c13CD0",
+    percentage: 0.039,
+    amount: 10,
+  },
+];
 
 export default function Gauge() {
-  const [keywords, setKeywords] = React.useState<string | undefined>(undefined);
-  // TODO: switch to use the new subgraph
+  const { isConnected } = useBeraJs();
+  const [keywords, setKeywords] = useState<string | undefined>(undefined);
   return (
     <div className="flex flex-col gap-12">
-      <div className="flex flex-col items-center gap-8 xs:gap-3 lg:flex-row">
+      <div className="xs:gap-3 flex flex-col gap-8 lg:flex-row">
         <GaugeInfoCard />
-        {/* {isLoading || !data || !data.length ? (
-          <div className="flex flex-col gap-16 md:flex-row">
-            <Skeleton className="h-[350px] w-[350px] flex-shrink-0 rounded-full" />
-          </div>
-        ) : ( */}
-        {/* <GlobalGaugeWeightChart gaugeWeights={[]} /> */}
-        {/* )} */}
-        <Skeleton className="h-[350px] w-[350px] flex-shrink-0 rounded-full" />
+        <GlobalGaugeWeightChart gaugeWeights={mockChartData} />
       </div>
-      <div className="flex flex-col">
-        <SearchInput
-          placeholder="Search..."
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setKeywords(e.target.value)
-          }
-        />
-        <div className="py-4">
-          {/* {isLoading || !data || !data.length ? (
-            <div className="mt-10 flex w-full flex-col gap-2">
-              <Skeleton className="h-10 w-full" />
-              <Skeleton className="h-10 w-full" />
-              <Skeleton className="h-10 w-full" />
-              <Skeleton className="h-10 w-full" />
-              <Skeleton className="h-10 w-full" />
-            </div>
-          ) : (
-            <GlobalGaugeWeightTable
-              gaugeWeights={data ?? []}
-              keywords={keywords}
+
+      <Tabs defaultValue="all-gauges" className="flex flex-col gap-4">
+        <div className="flex items-center justify-between md:flex-row flex-col gap-4">
+          <TabsList className="w-full md:w-fit">
+            <TabsTrigger value="all-gauges" className="w-full md:w-fit">
+              All Gauges
+            </TabsTrigger>
+            <TabsTrigger
+              value="my-gauges"
+              className="w-full md:w-fit"
+              disabled={!isConnected}
+            >
+              My Gauges
+            </TabsTrigger>
+          </TabsList>
+
+          <div className="flex items-center gap-3 w-full md:w-fit">
+            <SearchInput
+              placeholder="Search..."
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setKeywords(e.target.value)
+              }
+              className="w-full md:w-[300px]"
             />
-          )} */}
+            <MarketSelector />
+          </div>
         </div>
-      </div>
+        <TabsContent value="all-gauges">
+          <GlobalGaugeWeightTable
+            gaugeWeights={mockChartData}
+            keywords={keywords}
+          />
+        </TabsContent>
+        <TabsContent value="my-gauges">
+          <GlobalGaugeWeightTable
+            gaugeWeights={mockChartData}
+            keywords={keywords}
+          />
+        </TabsContent>
+      </Tabs>
     </div>
   );
 }
--- apps/bgt-station/src/components/chart-tooltip.tsx
+++ apps/bgt-station/src/components/chart-tooltip.tsx
@@ -1,51 +1,36 @@
-import { truncateHash, useGauges } from "@bera/berajs";
-import { getAddress } from "viem";
-
+import uniqolor from "uniqolor";
 import { GaugeIcon } from "~/app/validators/validators-table";
-import { OTHERS_GAUGES } from "./global-gauge-weight-chart";
 
-type ChartToolTipProps = {
-  color: string;
-  visible: boolean;
-  gauge: any;
-  numOthers?: number;
-};
-export function ChartTooltip({
-  color,
-  visible,
-  gauge,
-  numOthers,
-}: ChartToolTipProps) {
-  if (!visible) return null;
-  const { gaugeDictionary } = useGauges();
-  const name =
-    gauge.address === OTHERS_GAUGES
-      ? OTHERS_GAUGES
-      : gaugeDictionary
-        ? gaugeDictionary[getAddress(gauge?.address)]?.name
-        : "";
-  const otherInfo =
-    gauge.address === OTHERS_GAUGES
-      ? `${numOthers?.toString()} others`
-      : truncateHash(gauge?.address);
+export function ChartTooltip({ gauge }: { gauge: any }) {
+  if (!gauge) return null;
+  const otherInfo = "BERPS HONEY VAULT";
   return (
-    <div
-      className="z-1000 flex h-[68px] w-[320px] flex-row overflow-hidden rounded-md border bg-background"
-      style={{ border: `1px solid ${color}` }}
-    >
+    <div className="z-1000 flex min-w-[220px] gap-2 rounded-md border p-3 backdrop-blur-md">
       <div
-        className="text-md flex min-w-[90px] items-center justify-center font-semibold text-white"
-        style={{ backgroundColor: color }}
-      >
-        {gauge && (gauge?.percentage * 100).toFixed(2)}%
-      </div>
-      <div className="flex flex-col items-start justify-center p-3">
-        <div className="text-md text-forgeound flex flex-row items-center justify-center gap-2 font-semibold">
-          <GaugeIcon address={gauge?.address ?? ""} />
-          {name ?? "Test default gauge"}
+        className="h-20 w-1 rounded-full"
+        style={{ background: uniqolor(gauge.address).color }}
+      />
+      <div className="flex w-full flex-col justify-between whitespace-nowrap ">
+        <div className="flex items-center justify-between">
+          <div className="flex gap-1 text-sm font-semibold leading-5">
+            {(gauge.percentage * 100).toFixed(2)}%
+            <div className="text-sm font-normal leading-5 text-muted-foreground">
+              (4.46M BGT)
+            </div>
+          </div>
+          <div className="flex h-[18px] items-center rounded-xs bg-muted-foreground bg-opacity-10 p-1 text-[10px]">
+            #01
+          </div>
         </div>
-        <div className="flex items-start justify-center text-sm text-muted-foreground">
-          {otherInfo}
+        <div className="flex flex-col items-start justify-center gap-1">
+          <div className="text-forgeound flex flex-row items-center gap-2 whitespace-nowrap font-bold leading-5">
+            <GaugeIcon address={gauge.address} />
+            {gauge.name}
+          </div>
+          <div className="flex items-center gap-1 whitespace-nowrap text-xs leading-4 text-muted-foreground">
+            <GaugeIcon address={gauge.address} className="h-4 w-4" />{" "}
+            {otherInfo}
+          </div>
         </div>
       </div>
     </div>
