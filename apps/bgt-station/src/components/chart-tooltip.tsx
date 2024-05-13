1b7b13e1b1937bb3bcd21fdf4b8c3883afe03c30
Garrett Parris <garrettp18@gmail.com>
2024-05-13 10:53:50 -0400

    common interfaces, individual validator page (#957)

--- apps/bgt-station/src/components/chart-tooltip.tsx
+++ apps/bgt-station/src/components/chart-tooltip.tsx
@@ -1,35 +1,36 @@
 import uniqolor from "uniqolor";
 import { GaugeIcon } from "~/app/validators/validators-table";
+import { type CuttingBoardWeight } from "@bera/berajs";
+import { FormattedNumber } from "@bera/shared-ui";
 
-export function ChartTooltip({ gauge }: { gauge: any }) {
+export function ChartTooltip({ gauge }: { gauge: CuttingBoardWeight }) {
   if (!gauge) return null;
-  const otherInfo = "BERPS HONEY VAULT";
   return (
     <div className="z-1000 flex min-w-[220px] gap-2 rounded-md border p-3 backdrop-blur-md">
       <div
         className="h-20 w-1 rounded-full"
-        style={{ background: uniqolor(gauge.address).color }}
+        style={{ background: uniqolor(gauge.receiver.address).color }}
       />
       <div className="flex w-full flex-col justify-between whitespace-nowrap ">
         <div className="flex items-center justify-between">
           <div className="flex gap-1 text-sm font-semibold leading-5">
             {(gauge.percentage * 100).toFixed(2)}%
             <div className="text-sm font-normal leading-5 text-muted-foreground">
-              (4.46M BGT)
+              <FormattedNumber value={gauge.amount} compact /> BGT
             </div>
           </div>
-          <div className="flex h-[18px] items-center rounded-xs bg-muted-foreground bg-opacity-10 p-1 text-[10px]">
+          {/* <div className="flex h-[18px] items-center rounded-xs bg-muted-foreground bg-opacity-10 p-1 text-[10px]">
             #01
-          </div>
+          </div> */}
         </div>
         <div className="flex flex-col items-start justify-center gap-1">
           <div className="text-forgeound flex flex-row items-center gap-2 whitespace-nowrap font-bold leading-5">
-            <GaugeIcon address={gauge.address} />
-            {gauge.name}
+            <GaugeIcon gauge={gauge} />
+            {gauge.receiver.name}
           </div>
           <div className="flex items-center gap-1 whitespace-nowrap text-xs leading-4 text-muted-foreground">
-            <GaugeIcon address={gauge.address} className="h-4 w-4" />{" "}
-            {otherInfo}
+            <GaugeIcon gauge={gauge} className="h-4 w-4" />{" "}
+            {gauge.receiver.market.name}
           </div>
         </div>
       </div>
