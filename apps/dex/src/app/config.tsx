15ee6a4b2b9fc60e32d7c73f7a99bb9d4671fa3c
Pookie Bear <Khaledvsall@gmail.com>
2024-06-01 18:55:39 -0400

    fix: remove rewards from nav (#971)
    * fix wrapping switch bug
    
    * remove rewards from menu

--- apps/dex/src/app/config.tsx
+++ apps/dex/src/app/config.tsx
@@ -25,10 +25,6 @@ export const navItems = [
     href: "/pools",
     title: "Pool",
   },
-  {
-    href: "/rewards",
-    title: "Rewards",
-  },
   // {
   //   href: faucetUrl,
   //   title: "Faucet",
