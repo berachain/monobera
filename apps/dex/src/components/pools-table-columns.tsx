acb053fdef9276e5add76e515c6ec66d275a2f35
Garrett Parris <garrettp18@gmail.com>
2024-06-02 22:44:34 -0400

    refactor fee stuff, fix pool loading thing (#984)

--- apps/dex/src/components/pools-table-columns.tsx
+++ apps/dex/src/components/pools-table-columns.tsx
@@ -63,7 +63,7 @@ export const columns: ColumnDef<PoolV2>[] = [
     header: ({ column }) => (
       <DataTableColumnHeader
         column={column}
-        tooltip="Total amount of assets currently locked, in the Pool"
+        tooltip="Total amount of assets currently locked in the Pool, valued in HONEY"
         title="TVL"
         className="min-w-[95px]"
       />
@@ -85,7 +85,7 @@ export const columns: ColumnDef<PoolV2>[] = [
       <DataTableColumnHeader
         column={column}
         title="Fees (24H)"
-        tooltip="Total transaction fees this pool has generated in the last 24 hours"
+        tooltip="Total trading fees this pool has generated in the last 24 hours, valued in HONEY"
         className="whitespace-nowrap"
       />
     ),
@@ -127,8 +127,8 @@ export const columns: ColumnDef<PoolV2>[] = [
     header: ({ column }) => (
       <DataTableColumnHeader
         column={column}
-        title="BGT Rewards"
-        tooltip="Incentives distributed to users for providing liquidity to the pool."
+        title="BGT APY"
+        tooltip={apyTooltipText()}
         className="whitespace-nowrap"
       />
     ),
@@ -146,33 +146,33 @@ export const columns: ColumnDef<PoolV2>[] = [
     },
     filterFn: (row, id, value) => value.includes(row.getValue(id)),
   },
-  {
-    accessorKey: "totalApy",
-    enableSorting: false,
-    header: ({ column }) => (
-      <DataTableColumnHeader
-        column={column}
-        title="APY"
-        tooltip={apyTooltipText()}
-        className="whitespace-nowrap"
-      />
-    ),
-    cell: ({ row }) => {
-      return (
-        <div className="flex items-center justify-center text-sm">
-          <FormattedNumber value={row.original.totalApy ?? 0} percent colored />
-        </div>
-      );
-    },
-    sortingFn: (rowA, rowB) => {
-      const a = rowA.original.totalApy ?? 0;
-      const b = rowB.original.totalApy ?? 0;
-      if (a < b) return -1;
-      if (a > b) return 1;
-      return 0;
-    },
-    filterFn: (row, id, value) => value.includes(row.getValue(id)),
-  },
+  // {
+  //   accessorKey: "totalApy",
+  //   enableSorting: false,
+  //   header: ({ column }) => (
+  //     <DataTableColumnHeader
+  //       column={column}
+  //       title="APY"
+  //       tooltip={apyTooltipText()}
+  //       className="whitespace-nowrap"
+  //     />
+  //   ),
+  //   cell: ({ row }) => {
+  //     return (
+  //       <div className="flex items-center justify-center text-sm">
+  //         <FormattedNumber value={row.original.totalApy ?? 0} percent colored />
+  //       </div>
+  //     );
+  //   },
+  //   sortingFn: (rowA, rowB) => {
+  //     const a = rowA.original.totalApy ?? 0;
+  //     const b = rowB.original.totalApy ?? 0;
+  //     if (a < b) return -1;
+  //     if (a > b) return 1;
+  //     return 0;
+  //   },
+  //   filterFn: (row, id, value) => value.includes(row.getValue(id)),
+  // },
   {
     accessorKey: "btns",
     header: ({ column }) => (
@@ -247,8 +247,8 @@ export const my_columns: ColumnDef<IUserPool>[] = [
     header: ({ column }) => (
       <DataTableColumnHeader
         column={column}
-        title="BGT Rewards"
-        tooltip="Incentives distributed to users for providing liquidity to the pool."
+        title="BGT APY"
+        tooltip={apyTooltipText()}
         className="whitespace-nowrap"
       />
     ),
