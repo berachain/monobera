b4c6e891d1a55dc2d46880e47816ee50791496d8
Khaled Ali <Khaledvsall@gmail.com>
2024-06-05 14:44:09 -0400

    remove console

--- apps/dex/src/components/create-pool/create-pool-input.tsx
+++ apps/dex/src/components/create-pool/create-pool-input.tsx
@@ -19,7 +19,6 @@ export default function CreatePoolInput({
   const handleTokenSelection = (token: Token | undefined) => {
     onTokenSelection(token);
   };
-  console.log(token);
   return (
     <li className={"flex flex-row items-center justify-between gap-1 p-4"}>
       <SelectToken
