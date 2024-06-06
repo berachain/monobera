f4cfcc618fac569b4ce7f8b97f9187879ef38ef5
Garrett Parris <garrettp18@gmail.com>
2024-06-06 11:15:13 -0400

    bet (#1013)
    * bet
    
    * bet
    
    * bet
    
    ---------
    
    Signed-off-by: Garrett Parris <garrettp18@gmail.com>

--- apps/bgt-station/src/app/rewards/my-incentives-table.tsx
+++ apps/bgt-station/src/app/rewards/my-incentives-table.tsx
@@ -1,18 +1,15 @@
-import { UserValidator, useUserValidators } from "@bera/berajs";
+import { useUserActiveValidators } from "@bera/berajs";
 import { DataTable } from "@bera/shared-ui";
 import { user_incentives_columns } from "~/columns/general-validator-columns";
 
 export const MyIncentivesTableTable = () => {
-  const { data, isLoading } = useUserValidators();
-  const depositedValidators = data?.filter((validator: UserValidator) => {
-    return parseFloat(validator.userStaked) !== 0;
-  });
+  const { data, isLoading } = useUserActiveValidators();
   return (
     <div>
       <DataTable
         columns={user_incentives_columns}
         loading={isLoading}
-        data={depositedValidators ?? []}
+        data={data ?? []}
         className="min-w-[800px] shadow"
         enablePagination
       />
