027fee94d4d3e9c161ae00a2c221a72ada5367ee
ninibear030 <bina@berachain.com>
2024-06-07 15:53:43 -0400

    lint

--- apps/bgt-station/src/app/gauge/gauge.tsx
+++ apps/bgt-station/src/app/gauge/gauge.tsx
@@ -1,10 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import {
-  useBeraJs,
-  usePollGlobalData,
-} from "@bera/berajs";
+import { useBeraJs, usePollGlobalData } from "@bera/berajs";
 import { SearchInput } from "@bera/shared-ui";
 import { Icons } from "@bera/ui/icons";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@bera/ui/tabs";
