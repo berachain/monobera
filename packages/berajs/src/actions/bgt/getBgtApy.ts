2842654ef1aa4f559bcadcf3cec524f85731a081
ninibear030 <bina@berachain.com>
2024-06-10 11:18:19 -0400

    try fix

--- packages/berajs/src/actions/bgt/getBgtApy.ts
+++ packages/berajs/src/actions/bgt/getBgtApy.ts
@@ -14,88 +14,96 @@ interface GetBgtApyArgs {
  * fetch the current honey price of a given token
  */
 
+// export const getBgtApy = async ({
+//   receiptTokenAddress,
+//   tvlInHoney,
+//   config,
+// }: GetBgtApyArgs): Promise<string | undefined> => {
+//   if (!config.subgraphs?.bgtSubgraph) {
+//     throw new Error("bgt subgraph uri is not found in config");
+//   }
+//   if (!config.subgraphs?.dexSubgraph) {
+//     throw new Error("dex subgraph uri is not found in config");
+//   }
+//   const bgtClient = new ApolloClient({
+//     uri: config.subgraphs?.bgtSubgraph,
+//     cache: new InMemoryCache(),
+//   });
+
+//   const dexClient = new ApolloClient({
+//     uri: config.subgraphs?.dexSubgraph,
+//     cache: new InMemoryCache(),
+//   });
+
+//   if (!receiptTokenAddress) {
+//     return undefined;
+//   }
+//   if (!tvlInHoney || tvlInHoney === 0 || !Number.isFinite(tvlInHoney)) {
+//     return undefined;
+//   }
+
+//   const beraHoneyPrice = await dexClient
+//     .query({
+//       query: getTokenHoneyPriceReq,
+//       variables: {
+//         id: beraTokenAddress.toLowerCase(),
+//       },
+//     })
+//     .then((res: any) => {
+//       return res.data.tokenHoneyPrice?.price;
+//     })
+//     .catch((e: any) => {
+//       console.log(e);
+//       return "0";
+//     });
+//   const apyInfo: any = await bgtClient
+//     .query({
+//       query: getApyInfo,
+//     })
+//     .then((res: any) => {
+//       return res;
+//     })
+//     .catch((e: any) => {
+//       console.log(e);
+//       return undefined;
+//     });
+
+//   if (!apyInfo) return "0";
+
+//   const globalRewardRate =
+//     parseFloat(apyInfo.data.globalInfo.baseRate) +
+//     parseFloat(apyInfo.data.globalInfo.rewardRate);
+
+//   const totalBgtStaked = parseFloat(apyInfo.data.globalInfo.totalBgtStaked);
+
+//   const selectedCuttingBoard = apyInfo.data.globalCuttingBoardWeights.find(
+//     (cb: any) =>
+//       cb.vault.stakingToken.id.toLowerCase() ===
+//       receiptTokenAddress.toLowerCase(),
+//   );
+
+//   if (!selectedCuttingBoard) return "0";
+
+//   // if (selectedCuttingBoard.amount === "0") return "0";
+//   // const estimatedBgtPerBlock =
+//   //   (parseFloat(selectedCuttingBoard.amount) / totalBgtStaked) *
+//   //   globalRewardRate;
+//   // const secondsInAYear = 60 * 60 * 24 * 365;
+//   // const blocksPerSecond = 1 / blockTime;
+//   // const blocksPerYear = secondsInAYear * blocksPerSecond;
+
+//   // const estimatedBgtPerYear = estimatedBgtPerBlock * blocksPerYear;
+//   const estimatedBgtPerYear = 852585;
+//   const honeyValueEstimatedBgtPerYear =
+//     estimatedBgtPerYear * parseFloat(beraHoneyPrice);
+//   const apy = (honeyValueEstimatedBgtPerYear / tvlInHoney).toString();
+//   return apy;
+// };
+
 export const getBgtApy = async ({
   receiptTokenAddress,
   tvlInHoney,
   config,
 }: GetBgtApyArgs): Promise<string | undefined> => {
-  if (!config.subgraphs?.bgtSubgraph) {
-    throw new Error("bgt subgraph uri is not found in config");
-  }
-  if (!config.subgraphs?.dexSubgraph) {
-    throw new Error("dex subgraph uri is not found in config");
-  }
-  const bgtClient = new ApolloClient({
-    uri: config.subgraphs?.bgtSubgraph,
-    cache: new InMemoryCache(),
-  });
-
-  const dexClient = new ApolloClient({
-    uri: config.subgraphs?.dexSubgraph,
-    cache: new InMemoryCache(),
-  });
-
-  if (!receiptTokenAddress) {
-    return undefined;
-  }
-  if (!tvlInHoney || tvlInHoney === 0 || !Number.isFinite(tvlInHoney)) {
-    return undefined;
-  }
-
-  const beraHoneyPrice = await dexClient
-    .query({
-      query: getTokenHoneyPriceReq,
-      variables: {
-        id: beraTokenAddress.toLowerCase(),
-      },
-    })
-    .then((res: any) => {
-      return res.data.tokenHoneyPrice?.price;
-    })
-    .catch((e: any) => {
-      console.log(e);
-      return "0";
-    });
-  const apyInfo: any = await bgtClient
-    .query({
-      query: getApyInfo,
-    })
-    .then((res: any) => {
-      return res;
-    })
-    .catch((e: any) => {
-      console.log(e);
-      return undefined;
-    });
-
-  if (!apyInfo) return "0";
-
-  const globalRewardRate =
-    parseFloat(apyInfo.data.globalInfo.baseRate) +
-    parseFloat(apyInfo.data.globalInfo.rewardRate);
-
-  const totalBgtStaked = parseFloat(apyInfo.data.globalInfo.totalBgtStaked);
-
-  const selectedCuttingBoard = apyInfo.data.globalCuttingBoardWeights.find(
-    (cb: any) =>
-      cb.vault.stakingToken.id.toLowerCase() ===
-      receiptTokenAddress.toLowerCase(),
-  );
-
-  if (!selectedCuttingBoard) return "0";
-
-  // if (selectedCuttingBoard.amount === "0") return "0";
-  // const estimatedBgtPerBlock =
-  //   (parseFloat(selectedCuttingBoard.amount) / totalBgtStaked) *
-  //   globalRewardRate;
-  // const secondsInAYear = 60 * 60 * 24 * 365;
-  // const blocksPerSecond = 1 / blockTime;
-  // const blocksPerYear = secondsInAYear * blocksPerSecond;
-
-  // const estimatedBgtPerYear = estimatedBgtPerBlock * blocksPerYear;
-  const estimatedBgtPerYear = 852585;
-  const honeyValueEstimatedBgtPerYear =
-    estimatedBgtPerYear * parseFloat(beraHoneyPrice);
-  const apy = (honeyValueEstimatedBgtPerYear / tvlInHoney).toString();
-  return apy;
+   return "0";
 };
