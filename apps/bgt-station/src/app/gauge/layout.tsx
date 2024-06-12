eeacb684d9959367b7c9fd95bbd148e3aff84ef5
NINI BEAR <141682447+ninibear030@users.noreply.github.com>
2024-06-12 16:33:27 -0400

    reset input value && typo fix (#1124)

--- apps/bgt-station/src/app/gauge/layout.tsx
+++ apps/bgt-station/src/app/gauge/layout.tsx
@@ -3,7 +3,7 @@ import { Footer, FooterSM } from "@bera/shared-ui/src/footer";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <section>
-      <div className="container min-h-minimun pb-16">{children}</div>
+      <div className="container min-h-minimum pb-16">{children}</div>
       <Footer />
     </section>
   );
