d8a44c47e483014af5ef2ac365052e9bcb408d87
Khaled Ali <Khaledvsall@gmail.com>
2024-06-14 16:37:33 -0400

    add bee bg and improve fade slide logic

--- apps/bgt-station/src/app/dashboard/v2/how-to-earn-bgt.tsx
+++ apps/bgt-station/src/app/dashboard/v2/how-to-earn-bgt.tsx
@@ -2,6 +2,7 @@ import { FC, ReactNode, useEffect, useState } from "react";
 import { Shadows_Into_Light } from "next/font/google";
 import Link from "next/link";
 import { cloudinaryUrl, dexUrl, lendUrl, perpsUrl } from "@bera/config";
+import { FadeSlides } from "@bera/shared-ui/src/fade-slides";
 import { cn } from "@bera/ui";
 import { Icons } from "@bera/ui/icons";
 
@@ -38,75 +39,6 @@ export const HowToEarnCard: React.FC<HowToEarnCardProps> = ({
       </div>
       <div className="flex flex-grow translate-y-[-20%] flex-col justify-self-end">
         <img src={image} alt={title} className="h-[160px] object-contain" />
-      </div>
-    </div>
-  );
-};
-
-const FadeSlides: FC<{
-  slides: ReactNode[];
-  speed?: number;
-  currentSlide?: number;
-}> = ({ slides, speed = 1000, currentSlide }) => {
-  const [_currentSlide, set_CurrentSlide] = useState(currentSlide ?? 0);
-  const [isSlide1Shown, setIsSlide1Shown] = useState(true);
-  const [slide1Content, setSlide1Content] = useState(slides[0]);
-  const [slide2Content, setSlide2Content] = useState(slides[1] ?? slides[0]);
-
-  const handleSetSlide = (slideNumber: number) => {
-    if (!isSlide1Shown) {
-      setSlide1Content(slides[slideNumber]);
-      setIsSlide1Shown(true);
-    } else {
-      setSlide2Content(slides[slideNumber]);
-      setIsSlide1Shown(false);
-    }
-  };
-
-  useEffect(() => {
-    handleSetSlide(_currentSlide);
-  }, [_currentSlide]);
-
-  // This section is the logic for self controlled fade slides, if no currentSlide prop passed, it will cycle through given slides with the speed prop
-  const handleNextSlide = (nextSlide: number) => {
-    set_CurrentSlide(nextSlide);
-    setTimeout(() => {
-      handleNextSlide((nextSlide + 1) % slides.length);
-    }, speed);
-  };
-
-  useEffect(() => {
-    if (currentSlide !== undefined) return;
-    setTimeout(() => {
-      handleNextSlide(1);
-    }, speed);
-  }, []);
-
-  // This section is the logic for externally controlled fade slides, this allows the passing of a currentSlide prop to navigate to that slide index, if out of bounds will return null
-  useEffect(() => {
-    if (currentSlide === undefined) return;
-    set_CurrentSlide(currentSlide);
-  }, [currentSlide]);
-
-  if (
-    (isSlide1Shown && slide1Content === null) ||
-    (!isSlide1Shown && slide2Content === null)
-  )
-    return null;
-
-  return (
-    <div className="relative h-full w-full">
-      <div
-        className={`absolute bottom-0 left-0 right-0 top-0 z-[2] bg-background transition transition-opacity duration-500 ${
-          isSlide1Shown ? "" : "pointer-events-none opacity-0"
-        }`}
-      >
-        {slide1Content}
-      </div>
-      <div
-        className={"absolute bottom-0 left-0 right-0 top-0 z-[1] bg-background"}
-      >
-        {slide2Content}
       </div>
     </div>
   );
