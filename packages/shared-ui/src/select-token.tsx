2dbd17b06e83e38e965c03897d72150c679f81ce
NINI BEAR <141682447+ninibear030@users.noreply.github.com>
2024-06-18 12:45:55 -0400

    dex swap logic smol fix (#1213)

--- packages/shared-ui/src/select-token.tsx
+++ packages/shared-ui/src/select-token.tsx
@@ -33,7 +33,7 @@ export function SelectToken({
   const [open, setOpen] = React.useState(false);
 
   return (
-    <div className={cn("w-fit max-w-[150px] shadow", className)}>
+    <div className={cn("w-fit max-w-[150px]", className)}>
       <Button
         className="flex h-10 w-full shrink-0 gap-1 border-border bg-background p-2 text-secondary-foreground shadow"
         variant={"outline"}
