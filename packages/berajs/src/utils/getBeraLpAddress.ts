f612d7316bb4e23c566ffe1c123b889cf01adc73
ninibear030 <bina@berachain.com>
2024-06-19 14:22:25 -0400

    hide console log

--- packages/berajs/src/utils/getBeraLpAddress.ts
+++ packages/berajs/src/utils/getBeraLpAddress.ts
@@ -34,6 +34,6 @@ export const getBeraLpAddress = (base: string, quote: string) => {
     salt,
     initCodeHash,
   );
-  console.log("create2Address", create2Address);
+  // console.log("create2Address", create2Address);
   return create2Address;
 };
