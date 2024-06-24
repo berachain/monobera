1b6ef8cd7f99d16c471c24d535d1de30a01afb67
brown <brown@berachain.com>
2024-06-24 11:05:11 -0400

    feat(perps): refactor close all button and add to portfolio page

---
+++ apps/perp/src/app/components/close-all-orders.tsx
@@ -0,0 +1,167 @@
+import { useCallback, useContext, useMemo, type Dispatch } from "react";
+import { TransactionActionType, tradingAbi } from "@bera/berajs";
+import { Tooltip } from "@bera/shared-ui";
+import { useOctTxn } from "@bera/shared-ui/src/hooks";
+import { Button } from "@bera/ui/button";
+import { encodeFunctionData, type Address } from "viem";
+
+import { generateEncodedPythPrices } from "~/utils/formatPyth";
+import { createCloseOrderPayload } from "~/utils/generateCloseOrderPayload";
+import {
+  usePriceData,
+  usePythUpdateFeeFormatted,
+} from "~/context/price-context";
+import type {
+  CloseOrderPayload,
+  ILimitOrder,
+  IOpenTrade,
+} from "~/types/order-history";
+import type { TableStateProps } from "~/types/table";
+
+interface CloseAllOrdersProps {
+  tableState: TableStateProps;
+  setTableState: Dispatch<React.SetStateAction<TableStateProps>>;
+  orders: IOpenTrade[] | ILimitOrder[];
+  refetchMarketHistory: () => void;
+  refetchOrders?: () => void;
+  refetchPositions: () => void;
+}
+
+export const CloseAllOrders = ({
+  tableState,
+  setTableState,
+  orders,
+  refetchMarketHistory,
+  refetchOrders,
+  refetchPositions,
+}: CloseAllOrdersProps) => {
+  const pythUpdateFee = usePythUpdateFeeFormatted();
+
+  const prices = usePriceData();
+
+  const closePositionsPayload = useMemo<CloseOrderPayload[]>(() => {
+    return createCloseOrderPayload(orders, tableState.selection || {});
+  }, [orders, tableState.selection]);
+
+  const closeOrdersPayload = useMemo<CloseOrderPayload[]>(() => {
+    return createCloseOrderPayload(orders, tableState.selection || {});
+  }, [orders, tableState.selection]);
+
+  const {
+    isLoading: isClosePositionsLoading,
+    write: writePositionsClose,
+    ModalPortal: PositionsModal,
+  } = useOctTxn({
+    message: "Closing All Open Positions",
+    actionType: TransactionActionType.CANCEL_ALL_ORDERS,
+    onSuccess: () => {
+      setTableState((prev) => ({
+        ...prev,
+        selection: {},
+      }));
+      refetchPositions();
+      refetchMarketHistory();
+    },
+  });
+  const {
+    isLoading: isCloseLimitOrdersLoading,
+    write: writeOrdersClose,
+    ModalPortal: OrdersModal,
+  } = useOctTxn({
+    message: "Closing All Open Orders",
+    actionType: TransactionActionType.CANCEL_ALL_ORDERS,
+    onSuccess: () => {
+      setTableState((prev) => ({
+        ...prev,
+        selection: {},
+      }));
+      refetchOrders?.();
+      refetchMarketHistory();
+    },
+  });
+
+  const selectionSize = Object.keys(tableState.selection ?? {}).length;
+
+  const handleCloseAllPositions = useCallback(() => {
+    // TODO: update this to use the new multicall function when contracts are updated
+    const encodedData = closePositionsPayload.map((pos) => {
+      return encodeFunctionData({
+        abi: tradingAbi,
+        functionName: "closeTradeMarket",
+        args: [
+          pos.index,
+          generateEncodedPythPrices(prices, pos.pairIndex.toString()),
+        ],
+      });
+    });
+    writePositionsClose({
+      address: process.env.NEXT_PUBLIC_TRADING_CONTRACT_ADDRESS as Address,
+      abi: tradingAbi,
+      functionName: "multicall",
+      params: [true, encodedData],
+      value: pythUpdateFee * BigInt(closePositionsPayload.length),
+    });
+  }, [closePositionsPayload, prices, writePositionsClose]);
+
+  const handleCloseAllOrders = useCallback(() => {
+    const encodedData = closeOrdersPayload.map((order) => {
+      return encodeFunctionData({
+        abi: tradingAbi,
+        functionName: "cancelOpenLimitOrder",
+        args: [order.index],
+      });
+    });
+    writeOrdersClose({
+      address: process.env.NEXT_PUBLIC_TRADING_CONTRACT_ADDRESS as Address,
+      abi: tradingAbi,
+      functionName: "multicall",
+      params: [true, encodedData],
+    });
+  }, [closeOrdersPayload, writeOrdersClose]);
+
+  return (
+    <div>
+      {PositionsModal}
+      {OrdersModal}
+      {tableState.tabType === "positions" && (
+        <div className="flex flex-grow-0">
+          <Button
+            className="h-full w-fit min-w-0 cursor-pointer rounded-sm bg-destructive px-2 py-1 text-center text-sm font-semibold text-destructive-foreground hover:opacity-80"
+            disabled={
+              isClosePositionsLoading || closePositionsPayload?.length === 0
+            }
+            onClick={handleCloseAllPositions}
+          >
+            {`🌋 Close ${selectionSize ? selectionSize : "All"} Position${
+              selectionSize && selectionSize === 1 ? "" : "s"
+            }`}
+            <Tooltip
+              className="ml-1"
+              text="Can close a maximum of 10 positions at a time"
+            />
+          </Button>
+        </div>
+      )}
+
+      {tableState.tabType === "orders" && (
+        <div className="flex flex-grow-0">
+          <Button
+            className="h-full w-full min-w-44 cursor-pointer rounded-sm bg-destructive px-2 py-1 text-center text-sm font-semibold text-destructive-foreground hover:opacity-80 md:w-fit md:min-w-0"
+            disabled={
+              isCloseLimitOrdersLoading || closeOrdersPayload?.length === 0
+            }
+            onClick={handleCloseAllOrders}
+          >
+            {`🌋 Close ${selectionSize ? selectionSize : "All"} Order${
+              selectionSize && selectionSize === 1 ? "" : "s"
+            }`}
+            <Tooltip
+              className="ml-1"
+              text="Can close a maximum of 10 orders at a time"
+            />
+          </Button>
+        </div>
+      )}
+    </div>
+  );
+};
