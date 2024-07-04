0dc570cc47378a49f3b1657cc3ca208c80c8768f
ninibear030 <bina@berachain.com>
2024-07-04 12:36:31 -0400

    byebye vali clue

--- apps/validator-clue/src/types/iron-session/index.d.ts
+++
@@ -1,9 +0,0 @@
-import "iron-session";
-import { type SiweMessage } from "siwe";
-
-declare module "iron-session" {
-  interface IronSessionData {
-    nonce?: string;
-    siwe?: SiweMessage;
-  }
-}
