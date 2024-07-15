25517e9bdd0739a928c96b94e0da34339dd79b26
brown <brown@berachain.com>
2024-07-15 14:45:45 -0400

    task(faucet): migrate faucet from monobera Refs: BFE-105

--- apps/faucet/src/app/terms-of-use/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <section className="bg-sky-600 pt-10">
-      <div className="container min-h-minimum max-w-1280 rounded-lg bg-background pb-16 pt-10">
-        {children}
-      </div>
-    </section>
-  );
-}
