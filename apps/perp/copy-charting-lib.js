b4cb0685b32eff194d213b516a006066389aadd7
brown <brown@berachain.com>
2024-07-17 18:22:17 -0400

    task(perps): use private access token instead of public for trading view Refs: BFE-74

--- apps/perp/copy-charting-lib.js
+++ apps/perp/copy-charting-lib.js
@@ -1,16 +1,16 @@
 const shell = require("shelljs");
 const path = require("path");
 
-if (!process.env.NEXT_PUBLIC_PERPS_CHARTING_ACCESS_TOKEN) {
+if (!process.env.CHARTING_ACCESS_TOKEN) {
   console.log(
     "Charting library credentials are missing. Skipping the install of the charting library.",
     "\n\n",
     "To install the charting library, please provide the following environment variables:",
     "\n",
-    "NEXT_PUBLIC_PERPS_CHARTING_ACCESS_TOKEN",
+    "CHARTING_ACCESS_TOKEN",
   );
 } else {
   const scriptPath = path.join(__dirname, "copy-charting-lib.sh");
-  const command = `NEXT_PUBLIC_PERPS_CHARTING_ACCESS_TOKEN=${process.env.NEXT_PUBLIC_PERPS_CHARTING_ACCESS_TOKEN} ${scriptPath}`;
+  const command = `CHARTING_ACCESS_TOKEN=${process.env.CHARTING_ACCESS_TOKEN} ${scriptPath}`;
   shell.exec(command);
 }
