48c6c9694cb4227f5e7bc5589b145cd8ef0e25b6
ninibear030 <bina@berachain.com>
2024-07-18 11:34:38 -0400

    vote function

--- apps/bgt-station/src/app/governance/components/vote-dialog.tsx
+++ apps/bgt-station/src/app/governance/components/vote-dialog.tsx
@@ -1,8 +1,14 @@
 "use client";
 
 import React, { useState } from "react";
-import { VoteOption } from "@bera/proto/ts-proto-gen/cosmos-ts/cosmos/gov/v1/gov";
-import { ActionButton, Tooltip } from "@bera/shared-ui";
+import { GOVERNANCE_ABI, TransactionActionType } from "@bera/berajs";
+import { governorAddress } from "@bera/config";
+import {
+  ActionButton,
+  FormattedNumber,
+  Tooltip,
+  useTxn,
+} from "@bera/shared-ui";
 import { Button } from "@bera/ui/button";
 import {
   Dialog,
@@ -14,83 +20,103 @@ import {
 
 export function VoteDialog({
   votingPower,
+  proposalId,
 }: {
-  votingPower: number | undefined;
+  votingPower: string | undefined;
+  proposalId: string;
 }) {
   const [open, setOpen] = useState(false);
-  const [selected, setSelected] = useState(0);
+  // 0 = Against, 1 = For, 2 = Abstain
+  const [selected, setSelected] = useState(-1);
+
+  const { write, ModalPortal } = useTxn({
+    message: "Vote Proposal",
+    actionType: TransactionActionType.VOTE,
+    onSuccess: () => setOpen(false),
+  });
+
+  const vote = () =>
+    write({
+      address: governorAddress,
+      abi: GOVERNANCE_ABI,
+      functionName: "propose",
+      params: [proposalId, selected],
+    });
 
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <DialogTrigger asChild>
-        <Button
-          onClick={() => {
-            setOpen(true);
-          }}
-        >
-          Vote
-        </Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[420px]">
-        <DialogHeader>
-          <DialogTitle className="mb-3">Vote</DialogTitle>
-        </DialogHeader>
-        <div className="flex w-full flex-col items-center justify-center">
-          <p className="text-3xl font-semibold">
-            {votingPower ? `${(votingPower * 100).toFixed(8)}%` : "0%"}
-          </p>
-          <p className="text-sm font-medium">
-            Voting Power{" "}
-            <Tooltip
-              text={
-                "Represents the influence in network governance based on amount delegated to this validator"
-              }
+    <>
+      {ModalPortal}
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <ActionButton>
+            <Button
+              onClick={() => {
+                setOpen(true);
+              }}
+            >
+              Vote
+            </Button>
+          </ActionButton>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[420px]">
+          <DialogHeader>
+            <DialogTitle className="mb-3">Vote</DialogTitle>
+          </DialogHeader>
+          <div className="flex w-full flex-col items-center justify-center">
+            <FormattedNumber
+              value={votingPower ?? "0"}
+              symbol="BGT"
+              className="text-3xl font-semibold"
             />
-          </p>
-        </div>
 
-        <div className="flex w-full flex-col items-center justify-center gap-4">
-          <Button
-            variant={
-              selected === VoteOption.VOTE_OPTION_YES ? "primary" : "outline"
-            }
-            onClick={() => setSelected(VoteOption.VOTE_OPTION_YES)}
-            className={"w-full"}
-          >
-            Yes
-          </Button>
-          <Button
-            variant={
-              selected === VoteOption.VOTE_OPTION_NO ? "primary" : "outline"
-            }
-            onClick={() => setSelected(VoteOption.VOTE_OPTION_NO)}
-            className={"w-full"}
-          >
-            No
-          </Button>
-          <Button
-            variant={
-              selected === VoteOption.VOTE_OPTION_ABSTAIN
-                ? "primary"
-                : "outline"
-            }
-            onClick={() => setSelected(VoteOption.VOTE_OPTION_ABSTAIN)}
-            className={"w-full"}
-          >
-            Abstain
-          </Button>
-        </div>
-        <hr />
-        <ActionButton>
-          <Button
-            className="w-full"
-            disabled={votingPower === 0}
-            variant="success"
-          >
-            Submit
-          </Button>
-        </ActionButton>
-      </DialogContent>
-    </Dialog>
+            <p className="text-sm font-medium">
+              Voting Power{" "}
+              <Tooltip
+                text={
+                  "Represents the influence in network governance based on amount delegated to this validator"
+                }
+              />
+            </p>
+          </div>
+
+          <div className="flex w-full flex-col items-center justify-center gap-4">
+            <Button
+              variant={selected === 1 ? "primary" : "outline"}
+              onClick={() => setSelected(1)}
+              className={"w-full"}
+            >
+              Yes
+            </Button>
+            <Button
+              variant={selected === 0 ? "primary" : "outline"}
+              onClick={() => setSelected(0)}
+              className={"w-full"}
+            >
+              No
+            </Button>
+            <Button
+              variant={selected === 2 ? "primary" : "outline"}
+              onClick={() => setSelected(2)}
+              className={"w-full"}
+            >
+              Abstain
+            </Button>
+          </div>
+          <hr />
+          <ActionButton>
+            <Button
+              className="w-full"
+              disabled={
+                !votingPower || Number(votingPower) === 0 || selected === -1
+              }
+              variant="success"
+              onClick={vote}
+            >
+              Submit
+            </Button>
+          </ActionButton>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 }
--- apps/bgt-station/src/app/governance/proposal/[proposalId]/Status.tsx
+++ apps/bgt-station/src/app/governance/proposal/[proposalId]/Status.tsx
@@ -1,19 +1,25 @@
-import { type Proposal } from "@bera/berajs";
+import { usePollWalletBalances, type Proposal } from "@bera/berajs";
+import { bgtTokenAddress } from "@bera/config";
 
 import { VoteDialog } from "../../components/vote-dialog";
 import { StatusEnum } from "../../types";
 
 export const Status = ({ proposal }: { proposal: Proposal }) => {
+  const { useSelectedWalletBalance } = usePollWalletBalances();
+  const bgt = useSelectedWalletBalance(bgtTokenAddress);
   const status = proposal.status as StatusEnum;
   return (
     <div className="flex items-center gap-3 font-medium">
-      {/* {ModalPortal} */}
-
       {status === StatusEnum.PENDING && (
         <div>Voting starts at {proposal.start.timestamp}</div>
       )}
       {status === StatusEnum.QUEUED && <div>Voting in queue</div>}
-      {status === StatusEnum.ACTIVE && <VoteDialog votingPower={1} />}
+      {status === StatusEnum.ACTIVE && (
+        <VoteDialog
+          votingPower={bgt?.formattedBalance}
+          proposalId={proposal.onchainId}
+        />
+      )}
       {status === StatusEnum.CANCELED && <div>Canceled</div>}
       {status === StatusEnum.SUCCEEDED && (
         <div className="text-success-foreground">Succeded</div>
