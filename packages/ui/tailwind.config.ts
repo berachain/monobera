1aab245101a9520bd99c26e1c1af2476575731ee
brown <brown@berachain.com>
2024-07-25 17:21:23 -0400

    add recharts and shadcn charts

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -4,4 +4,27 @@ import type { Config } from "tailwindcss";
 export default {
   content: baseConfig.content,
   presets: [baseConfig],
+  theme: {
+    aspectRatio: {
+      auto: "auto",
+      square: "1 / 1",
+      video: "16 / 9",
+      1: "1",
+      2: "2",
+      3: "3",
+      4: "4",
+      5: "5",
+      6: "6",
+      7: "7",
+      8: "8",
+      9: "9",
+      10: "10",
+      11: "11",
+      12: "12",
+      13: "13",
+      14: "14",
+      15: "15",
+      16: "16",
+    },
+  },
 } satisfies Config;
