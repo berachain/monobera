42f8b40dfff879c5bb018611c8a7df3fd74536c5
ninibear030 <bina@berachain.com>
2024-08-30 16:29:53 -0400

    bash

--- script/check_diff.sh
+++ script/check_diff.sh
@@ -1,1 +1,14 @@
-echo 'test'
+if git ls-tree -d HEAD --name-only | grep -q "$DIR_PATTERN"; then
+  # If it exists, check for changes between the latest commit and its parent
+  git diff HEAD^ HEAD --quiet -- "${DIR_PATTERN//^/}"
+  if [ $? -eq 1 ]; then
+    echo "Changes detected in ${DIR_PATTERN//^/}"
+    exit 1
+  else
+    echo "No changes in ${DIR_PATTERN//^/}"
+    exit 0
+  fi
+else
+  echo "Directory ${DIR_PATTERN//^/} does not exist in the current commit"
+  exit 0
+fi
