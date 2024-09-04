e6773de7513b208dfc833be9dd3042e974fac2f5
bearpong <178402093+bearpong@users.noreply.github.com>
2024-09-04 13:56:36 +0200

    chore: enable sentry only on vercel

--- apps/honey/sentry.server.config.ts
+++ apps/honey/sentry.server.config.ts
@@ -5,6 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
+  enabled: process.env.VERCEL === "1",
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
   // Adjust this value in production, or use tracesSampler for greater control
