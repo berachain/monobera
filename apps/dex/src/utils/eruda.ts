ef45db7eb154f85ba74319e93a26959a021ae21e
ninibear030 <bina@berachain.com>
2024-09-10 12:15:22 -0400

    try

--- apps/dex/src/utils/eruda.ts
+++ apps/dex/src/utils/eruda.ts
@@ -1,6 +1,6 @@
 export const initEruda = () => {
   // Check if the window object exists and if it's in a development environment
-  if (typeof window !== "undefined" && process.env.NODE_ENV !== "production") {
+  if (typeof window !== "undefined") {
     const eruda = require("eruda");
     eruda.init();
   }
