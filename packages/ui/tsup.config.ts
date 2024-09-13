2f66651b088f057df4ff2d1eeec505760c9d47e0
bearpong <178402093+bearpong@users.noreply.github.com>
2024-09-13 11:22:19 +0200

    chore: removed unused base style.css

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -37,7 +37,7 @@ const client = [
   "./src/slider.tsx",
   "./src/chart.tsx",
   "./src/recharts.tsx",
-  "./src/pagination.tsx"
+  "./src/pagination.tsx",
 ];
 
 const server = [
@@ -82,7 +82,6 @@ export default defineConfig((opts) => {
         ) as PackageJson;
         pkgJson.exports = {
           "./package.json": "./package.json",
-          "./styles.css": "./dist/index.css",
           ".": {
             import: "./dist/index.mjs",
             types: "./dist/index.d.ts",
