3619447f791e7fd4820edcea2978efc7f9dcc44d
bearpong <178402093+bearpong@users.noreply.github.com>
2024-09-13 19:05:45 +0200

    feat(hub): redesigned delegation flow

--- apps/hub/src/app/governance/components/governance-provider.tsx
+++ apps/hub/src/app/governance/components/governance-provider.tsx
@@ -1,17 +1,36 @@
 "use client";
+import BigNumber from "bignumber.js";
 
 import { ApolloProvider } from "@apollo/client";
-import { usePollProposalThreshold, usePollUserDelegates } from "@bera/berajs";
+import {
+  useBeraJs,
+  usePollBalance,
+  usePollProposalThreshold,
+  usePollUserDelegates,
+} from "@bera/berajs";
 import { bgtClient } from "@bera/graphql";
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { Address } from "viem";
-import { governorAddress as governorAddressFromEnv } from "@bera/config";
+import {
+  governanceTokenAddress,
+  governorAddress as governorAddressFromEnv,
+} from "@bera/config";
 import { NotEnoughVotingPower } from "./not-enough-voting-power";
+import { DelegateModal } from "../[genre]/components/delegate-modal";
 type GovernanceContextType = {
   canPropose: boolean;
   votesThreshold?: string;
+  /**
+   * The voting delegated to the user, by themselves or others
+   */
   currentVotes?: string;
+  /**
+   * The BGT token balance of the user
+   */
+  tokenBalance: string | undefined;
   isLoading: boolean;
+  delegatedToThemselves: string;
+  delegatedByOthers: string;
   governorAddress: Address;
   openNotEnoughVotingPowerDialog: (args?: {
     onClose?: () => void;
@@ -37,8 +56,18 @@ const _GovernanceProvider = ({ children }: { children: React.ReactNode }) => {
   const [governorAddress, setGovernorAddress] = useState<Address>(
     governorAddressFromEnv,
   );
+  const { account } = useBeraJs();
+  const { data: tokenBalanceData, isLoading: isLoadingTokenBalance } =
+    usePollBalance({ address: governanceTokenAddress });
 
   const [isDialogOpen, setIsDialogOpen] = useState<{
+    isOpen: boolean;
+    onClose: () => void;
+  }>({
+    isOpen: false,
+    onClose: () => {},
+  });
+  const [isDelegateModalOpen, setIsDelegateModalOpen] = useState<{
     isOpen: boolean;
     onClose: () => void;
   }>({
@@ -49,7 +78,27 @@ const _GovernanceProvider = ({ children }: { children: React.ReactNode }) => {
   const { data: voteData, isLoading: isLoadingVotes } = usePollUserDelegates();
   const { data: votesThresholdData, isLoading: isLoadingVotesThreshold } =
     usePollProposalThreshold(governorAddress!);
+  const canPropose =
+    !!voteData &&
+    !!votesThresholdData &&
+    Number(voteData.currentVotes) >= Number(votesThresholdData.votesThreshold);
 
+  const delegatedToThemselves =
+    tokenBalanceData && voteData?.delegate === account
+      ? tokenBalanceData.formattedBalance
+      : "0";
+  const delegatedByOthers =
+    voteData?.currentVotes && tokenBalanceData
+      ? BigNumber(voteData.currentVotes)
+          .minus(BigNumber(delegatedToThemselves))
+          .toString()
+      : "0";
+
+  useEffect(() => {
+    if (canPropose) {
+      setIsDialogOpen((prev) => ({ ...prev, isOpen: false }));
+    }
+  }, [canPropose]);
   return (
     <GovernanceContext.Provider
       value={
@@ -60,19 +109,22 @@ const _GovernanceProvider = ({ children }: { children: React.ReactNode }) => {
           } = {}) => {
             setIsDialogOpen((prev) => ({ ...prev, isOpen, onClose }));
           },
-          isLoading: isLoadingVotes || isLoadingVotesThreshold,
+          isLoading:
+            isLoadingVotes || isLoadingVotesThreshold || isLoadingTokenBalance,
           governorAddress,
-          canPropose:
-            !!voteData &&
-            !!votesThresholdData &&
-            Number(voteData.currentVotes) >=
-              Number(votesThresholdData.votesThreshold),
+          delegatedByOthers,
+          delegatedToThemselves,
+          tokenBalance: tokenBalanceData?.formattedBalance,
+          canPropose,
           votesThreshold: votesThresholdData?.votesThreshold,
           currentVotes: voteData?.currentVotes,
         } as GovernanceContextType
       }
     >
       <NotEnoughVotingPower
+        onOpenDelegate={() =>
+          setIsDelegateModalOpen((prev) => ({ ...prev, isOpen: true }))
+        }
         isOpen={isDialogOpen.isOpen}
         onOpenChange={(isOpen) => {
           !isOpen && isDialogOpen.onClose?.();
@@ -83,6 +135,21 @@ const _GovernanceProvider = ({ children }: { children: React.ReactNode }) => {
         }}
         votesThreshold={votesThresholdData?.votesThreshold}
         currentVotes={voteData?.currentVotes}
+      />
+      <DelegateModal
+        isOpen={isDelegateModalOpen.isOpen}
+        onOpenChange={(isOpen) => {
+          !isOpen && isDelegateModalOpen.onClose?.();
+
+          setIsDelegateModalOpen((prev) => ({
+            ...prev,
+            isOpen,
+          }));
+          setIsDialogOpen((prev) => ({
+            ...prev,
+            isOpen: true,
+          }));
+        }}
       />
       {children}
     </GovernanceContext.Provider>
