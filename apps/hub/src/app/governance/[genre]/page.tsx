bd68ee97aa32e86d4228454461963ed6c8dec96a
bearpong <178402093+bearpong@users.noreply.github.com>
2024-09-16 14:53:26 +0200

    feat(hub): genre in proposal body

--- apps/hub/src/app/governance/[genre]/page.tsx
+++ apps/hub/src/app/governance/[genre]/page.tsx
@@ -3,7 +3,11 @@
 import React from "react";
 import { notFound } from "next/navigation";
 
-import { PROPOSAL_GENRE, getDappByGenre } from "../governance-genre-helper";
+import {
+  PROPOSAL_GENRE,
+  getDappByGenre,
+  isValidGenre,
+} from "../governance-genre-helper";
 import GovernanceByStatus from "./components/governance-by-status";
 import { governorAddress } from "@bera/config";
 
@@ -12,8 +16,7 @@ export default function Page({
 }: {
   params: { genre: PROPOSAL_GENRE };
 }) {
-  if (!["berahub", "honey", "bend", "berps", "general"].includes(params.genre))
-    return notFound();
+  if (!isValidGenre(params.genre)) return notFound();
 
   return (
     <GovernanceByStatus
