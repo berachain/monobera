db3d87cd28e8fbc1575e9775f835c0679edc79b2
bearpong <178402093+bearpong@users.noreply.github.com>
2024-10-22 14:31:39 +0200

    fix(bgt): should fix #131

--- packages/graphql/src/modules/pol/index.ts
+++ packages/graphql/src/modules/pol/index.ts
@@ -2,40 +2,40 @@ import { gql } from "@apollo/client";
 
 export const GetTotalBgtDistributed = gql`
   query {
-    globalInfo(id:"global") {
-        id
-        totalBGTDistributed
+    globalInfo(id: "global") {
+      id
+      totalBGTDistributed
     }
-}
+  }
 `;
 
 export const GetBgtInflation = gql`
   query {
-    globalInfo(id:"global") {
-        id
-        rewardRate
-        baseRewardRate
+    globalInfo(id: "global") {
+      id
+      rewardRate
+      baseRewardRate
     }
-}
+  }
 `;
 
 export const GetUserValidatorInformation = gql`
   query GetUserValidatorInformation($address: String!) {
-    userValidatorInformations(where: { user: $address }) {
-    id
-    amountQueued
-    amountDeposited
-    latestBlock
-    user
-    coinbase
-  }
+    userValidatorInformations(where: { user: $address, limit: 1000 }) {
+      id
+      amountQueued
+      amountDeposited
+      latestBlock
+      user
+      coinbase
+    }
   }
 `;
 
 export const GetValidValidator = gql`
-query GetValidValidator($address: String!) {
-  validator(id: $address) {
-    coinbase
+  query GetValidValidator($address: String!) {
+    validator(id: $address) {
+      coinbase
+    }
   }
-}
 `;
